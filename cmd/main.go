47a3184ab5c4cba2374e63ddbbac85d181379035
natnael jemaneh <njemaneh@chegebeya.com>
2025-01-27 14:28:35 +0300

    + env file, + env configuration from os.args

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import "flag"
+
+type config struct {
+	port int
+	env  string
+}
+
+func main() {
+	print("hello")
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	// flag.StringVar(&cfg.keycloakInstanceURL, "keycloak_base_url", "keycloak Instance Base URL", "Environment (development|staging|production)")
+	// flag.StringVar(&cfg.keycloakUsername, "keycloak_user_name", "keycloak Instance Base URL", "Environment (development|staging|production)")
+	// flag.StringVar(&cfg.keycloakPassword, "keycloak_password", "keycloak password", "Environment (development|staging|production)")
+	// flag.StringVar(&cfg.keycloakRealm, "keycloak_realm", "keycloak realm", "Environment (development|staging|production)")
+	// flag.StringVar(&cfg.keycloakApplicationRealm, "keycloak_application_realm", "keycloak application realm", "Environment (development|staging|production)")
+	// flag.StringVar(&cfg.keycloakClientId, "keycloak_client_id", "keycloak ClientId", "Environment (development|staging|production)")
+	// flag.StringVar(&cfg.keycloakClientSecret, "keycloak_client_secret", "keycloak ClientSecret", "Environment (development|staging|production)")
+
+	flag.Parse()
+}
