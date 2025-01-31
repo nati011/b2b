0bab285f37d9a3d3284a253b54e7b5085ffbe4df
natnael jemaneh <njemaneh@chegebeya.com>
2025-01-31 19:03:17 +0300

    + failing authTestContainer

--- cmd/auth/tests/testContainer.go
+++ cmd/auth/tests/testContainer.go
@@ -1,15 +1,41 @@
 package test
 
-type TestContainer struct {
+import (
+	"context"
+
+	auth "b2b.nati011.github.com/cmd/auth/service"
+	keycloak "github.com/stillya/testcontainers-keycloak"
+)
+
+var authTestContainer = &AuthTestContainer{
+	authService: &auth.AuthService{},
 }
 
-func (TestContainer) CreateTestContainer() (*TestContainer, error) {
-	tc := TestContainer{}
-	initTestContainer(&tc)
+type AuthTestContainer struct {
+	authService *auth.AuthService
+}
+
+func (AuthTestContainer) NewTestContainer() (*AuthTestContainer, error) {
+	tc := AuthTestContainer{}
+	err := initTestContainer(&tc)
+	if err != nil {
+		return nil, err
+	}
 	return &tc, nil
 }
 
-func initTestContainer(tc *TestContainer) error {
-	// initDependencies...
+func initTestContainer(tc *AuthTestContainer) error {
+	ctx := context.Background()
+	CreateKeycloakContainer(ctx)
 	return nil
+}
+
+func CreateKeycloakContainer(ctx context.Context) (*keycloak.KeycloakContainer, error) {
+	return keycloak.Run(ctx,
+		"keycloak/keycloak:24.0",
+		keycloak.WithContextPath("/auth"),
+		keycloak.WithRealmImportFile("../testdata/realm-export.json"),
+		keycloak.WithAdminUsername("admin"),
+		keycloak.WithAdminPassword("admin"),
+	)
 }
