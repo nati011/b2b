0bd3fd086ea4d77320b9cbc8aa9bad3d5d27b581
natnael jemaneh <njemaneh@chegebeya.com>
2025-01-31 19:03:17 +0300

    + failing authTestContainer

--- cmd/auth/service/auth.go
+++ cmd/auth/service/auth.go
@@ -1,10 +1,12 @@
 package auth
 
-import auth "b2b.nati011.github.com/cmd/auth/model/dto"
+import (
+	auth "b2b.nati011.github.com/cmd/auth/model/dto"
+)
 
 type AuthService struct {
 }
 
-func (a *AuthService) CreateClient() (auth.RegisterUserResponse, error) {
+func (a *AuthService) CreateClient(auth.RegisterUserRequest) (auth.RegisterUserResponse, error) {
 	return auth.RegisterUserResponse{}, nil
 }
