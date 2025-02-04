d39449a9e5a2574e194446237adb3ccd253ec3a2
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-04 15:27:39 +0300

    + di container in auth module

--- cmd/auth/provider/authProvider.go
+++ cmd/auth/provider/authProvider.go
@@ -1,6 +1,12 @@
 package auth
 
+type LoginAuthResonse struct {
+}
+
+type CreateClientAuthResonse struct {
+}
+
 type AuthProvider interface {
-	CreateNewClient(firstName string, lastName string, email string, username string, password string) (string, error)
-	ClientLogin(email, password string) (string, error)
+	CreateNewClient(firstName string, lastName string, email string, username string, password string) (CreateClientAuthResonse, error)
+	ClientLogin(email, password string) (LoginAuthResonse, error)
 }
--- cmd/auth/service/auth.go
+++ cmd/auth/service/auth.go
@@ -1,8 +1,9 @@
 package auth
 
 import (
+	"log"
+
 	auth "b2b.nati011.github.com/cmd/auth/model/dto"
-	provider "b2b.nati011.github.com/cmd/auth/provider"
 )
 
 type Authorizer interface {
@@ -14,10 +15,22 @@ type AuthService struct {
 	authProvider *provider.AuthProvider
 }
 
-func NewAuthService() (*AuthService, error) {
-	return &AuthService{}, nil
+func NewAuthService(ap *provider.AuthProvider) AuthService {
+	return AuthService{authProvider: ap}
 }
 
-func (a *AuthService) CreateClient(auth.RegisterUserRequest) (auth.RegisterUserResponse, error) {
-	return auth.RegisterUserResponse{}, nil
+func (a *AuthService) CreateClient(rq auth.RegisterUserRequest) (auth.RegisterUserResponse, error) {
+	resp, err := a.authProvider.CreateNewClient(rq.FullName, nil, nil, rq.Email, rq.Username, rq.Password)
+	if err != nil {
+		log.Fatalf("failed to create client")
+	}
+	return resp, nil
+}
+
+func (a *AuthService) LoginClient(rq auth.LoginUserRequest) (auth.LoginUserResonse, error) {
+	resp, err := a.authProvider
+	if err != nil {
+		log.Fatalf("failed to create client")
+	}
+	return resp, nil
 }
