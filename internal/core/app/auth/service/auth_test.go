f8295f90632513381b4da379417fa9eeed328a49
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-04 19:05:24 +0300

    + auth decides to go back to an old...

--- internal/core/app/auth/service/auth_test.go
+++ internal/core/app/auth/service/auth_test.go
@@ -3,8 +3,8 @@ package auth
 import (
 	"testing"
 
-	authDTO "b2b.nati011.github.com/pkg/auth/model/dto"
-	"b2b.nati011.github.com/pkg/auth/provider"
+	authDTO "b2b.nati011.github.com/internal/core/app/auth/model/dto"
+	"b2b.nati011.github.com/internal/core/app/auth/provider"
 )
 
 const (
