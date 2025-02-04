7b071e9362667b1054eec15e8a4d245635821dc9
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-04 16:20:11 +0300

    + cleanup error messages

--- cmd/auth/provider/authProvider.go
+++ cmd/auth/provider/authProvider.go
@@ -4,15 +4,27 @@ import "errors"
 
 // system errors
 var (
-	ErrUsernameTaken = errors.New("invalid username")
-	ErrEmailTaken    = errors.New("invalid email")
-	ErrFailedToLogin = errors.New("invalid email or password")
+	System_Readable_Error_UsernameTaken = errors.New("invalid username")
+	System_Readable_Error_EmailTaken    = errors.New("invalid email")
+	System_Readable_Error_FailedToLogin = errors.New("invalid email or password")
 )
 
 type LoginAuthResonse struct {
+	JWT struct {
+		AccessToken      string `json:"access_token"`
+		IDToken          string `json:"id_token"`
+		ExpiresIn        int    `json:"expires_in"`
+		RefreshExpiresIn int    `json:"refresh_expires_in"`
+		RefreshToken     string `json:"refresh_token"`
+		TokenType        string `json:"token_type"`
+		NotBeforePolicy  int    `json:"not-before-policy"`
+		SessionState     string `json:"session_state"`
+		Scope            string `json:"scope"`
+	}
 }
 
 type CreateClientAuthResonse struct {
+	Username string
 }
 
 type AuthProvider interface {
