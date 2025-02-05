c324b3d7eb0e6bd0157540437987cd2043863f12
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-05 09:44:12 +0300

    + fix package name, resolve issues caused by refactoring

--- internal/core/domain/auth/service/container.go
+++ internal/core/domain/auth/service/container.go
@@ -1,7 +1,7 @@
 package auth
 
 import (
-	provider "b2b.nati011.github.com/internal/core/app/auth/provider"
+	provider "b2b.nati011.github.com/internal/core/domain/auth/provider"
 )
 
 type Container struct {
