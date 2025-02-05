458e8c7c8022279546321e50e8262113fa319e14
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-05 09:44:12 +0300

    + fix package name, resolve issues caused by refactoring

--- internal/core/domain/auth/service/auth_test.go
+++ internal/core/domain/auth/service/auth_test.go
@@ -3,8 +3,8 @@ package auth
 import (
 	"testing"
 
-	authDTO "b2b.nati011.github.com/internal/core/app/auth/model/dto"
-	"b2b.nati011.github.com/internal/core/app/auth/provider"
+	authDTO "b2b.nati011.github.com/internal/core/domain/auth/model/dto"
+	"b2b.nati011.github.com/internal/core/domain/auth/provider"
 )
 
 const (
