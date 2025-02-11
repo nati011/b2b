8a0eacc9d5d784aac74a0fd046f8a9d80fd77c15
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-11 11:48:01 +0300

    + wrote failing tests

--- internal/core/domain/email/service/email.go
+++ internal/core/domain/email/service/email.go
@@ -1,1 +1,34 @@
 package email
+
+import "errors"
+
+type Request struct {
+	Addr    string
+	Content string
+	Header  string
+}
+
+type Response struct {
+	Addr    string
+	Message string
+}
+
+var (
+	ErrAddressNotValid = errors.New("oopsy, email is not valid")
+	ErrContentEmpty    = errors.New("oopsy, email content is empty")
+)
+
+type Emailer interface {
+	SendEmail(Request) (Response, error)
+}
+
+type EmailService struct {
+}
+
+func NewEmailService() *EmailService {
+	return &EmailService{}
+}
+
+func (e *EmailService) SendEMail(Request) (Response, error) {
+	return Response{}, nil
+}
