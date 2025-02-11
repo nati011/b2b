be11db382196fefa5711e19f721b81ec4a744e53
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-11 11:57:00 +0300

    + added email mock, it always succedes

--- internal/core/domain/email/provider/mockEmailProvider.go
+++ internal/core/domain/email/provider/mockEmailProvider.go
@@ -1,1 +1,13 @@
 package email
+
+import emailDTO "b2b.nati011.github.com/internal/core/domain/email/service"
+
+type MockEmailProvider struct {
+}
+
+func (m *MockEmailProvider) Send(e emailDTO.Request) (emailDTO.Response, error) {
+	return emailDTO.Response{
+		Addr:    e.Addr,
+		Message: emailDTO.SUCCESS_MESSAGE,
+	}, nil
+}
