eaa1c890f0b7b8f3991511eb02222b3feaba11eb
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-11 11:48:01 +0300

    + wrote failing tests

--- internal/core/domain/email/service/email_test.go
+++ internal/core/domain/email/service/email_test.go
@@ -1,1 +1,91 @@
 package email
+
+import (
+	"os"
+	"testing"
+)
+
+const (
+	SUCCESS_MESSAGE = "Ahoy, mail received!"
+)
+
+const (
+	VALID_EMAIL_ADDR   = "ruthtirusew944@mailpit.com"
+	INVALID_EMAIL_ADDR = "ruthtirusew944"
+	VALID_HEADER       = "test"
+	VALID_CONTENT      = "test"
+)
+
+var service *EmailService
+
+func Test_SendEmail_happyPath(t *testing.T) {
+	in := Request{
+		Addr:    VALID_EMAIL_ADDR,
+		Header:  VALID_HEADER,
+		Content: VALID_CONTENT,
+	}
+
+	want := Response{
+		Addr:    VALID_EMAIL_ADDR,
+		Message: SUCCESS_MESSAGE,
+	}
+
+	got, err := service.SendEMail(in)
+	if err != nil {
+		t.Errorf("Failed to send email err: %v", err)
+	}
+	if got != want {
+		t.Errorf("Expected: %v, Got: %v", want, got)
+	}
+}
+
+func Test_SendEmail_unhappyPath(t *testing.T) {
+	t.Run("invalidEmail", func(t *testing.T) {
+		in := Request{
+			Addr:    INVALID_EMAIL_ADDR,
+			Header:  VALID_HEADER,
+			Content: VALID_CONTENT,
+		}
+		want := Response{
+			Addr:    INVALID_EMAIL_ADDR,
+			Message: ErrAddressNotValid.Error(),
+		}
+		got, err := service.SendEMail(in)
+		if err != nil {
+			t.Errorf("Failed to send email err: %v", err)
+		}
+		if got != want {
+			t.Errorf("Expected: %v, Got: %v", want, got)
+		}
+
+	})
+
+	t.Run("emptyContent", func(t *testing.T) {
+		in := Request{
+			Addr:    VALID_EMAIL_ADDR,
+			Header:  VALID_HEADER,
+			Content: VALID_CONTENT,
+		}
+		want := Response{
+			Addr:    VALID_EMAIL_ADDR,
+			Message: ErrContentEmpty.Error(),
+		}
+		got, err := service.SendEMail(in)
+		if err != nil {
+			t.Errorf("Failed to send email err: %v", err)
+		}
+		if got != want {
+			t.Errorf("Expected: %v, Got: %v", want, got)
+		}
+	})
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	os.Exit(code)
+}
+
+func setup() {
+	service = NewEmailService()
+}
