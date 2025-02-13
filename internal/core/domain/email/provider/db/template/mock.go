a0a8bffe124817f213a1ae471c217cd269c7f68c
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-13 12:03:13 +0300

    + failing tests

--- internal/core/domain/email/provider/db/template/mock.go
+++ internal/core/domain/email/provider/db/template/mock.go
@@ -1,1 +1,33 @@
 package email
+
+type MockDB struct {
+	templates []template
+}
+
+func (m *MockDB) Create(r *CreateRequest) (CreateResponse, error) {
+	for _, i := range m.templates {
+		if i.name == r.Name {
+			return CreateResponse{}, ErrSysDuplicateName_L1
+		}
+	}
+	m.templates = append(m.templates, template{
+		name: r.Name,
+		html: r.HtmlTemplate,
+	})
+	return CreateResponse{
+		Name: r.Name,
+	}, nil
+}
+
+func (m *MockDB) Get(r string) (GetResponse, error) {
+	for _, i := range m.templates {
+		if i.name == r {
+			return GetResponse{}, nil
+		}
+	}
+	return GetResponse{}, nil
+}
+
+func (m *MockDB) GetAll() (GetAllResponse, error) {
+	return GetAllResponse{}, nil
+}
