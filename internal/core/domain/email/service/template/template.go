42924b5f7ed92a85b92c8b1cb66abe412cc13104
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-13 12:03:13 +0300

    + failing tests

--- internal/core/domain/email/service/template/template.go
+++ internal/core/domain/email/service/template/template.go
@@ -1,40 +1,107 @@
 package email
 
+import (
+	"errors"
+
+	db "b2b.nati011.github.com/internal/core/domain/email/provider/db/template"
+)
+
+var (
+	ErrSysInvalidTemplate = errors.New("template empty")
+	ErrSysInvalidName     = errors.New("empty name")
+	ErrSysUnknown         = errors.New("unknown")
+	ErrSysDuplicateName   = errors.New("duplicate name")
+)
+
+const (
+	SUCCESS_MESSAGE = "Ahoy, template created!"
+)
+
 type CreateRequest struct {
 	Name         string
 	HtmlTemplate string
 }
 
 type CreateResponse struct {
-	Id   string
-	Name string
+	Name    string
+	Message string
 }
 
 type GetResponse struct {
-	Name string
+	Name         string
+	HtmlTemplate string
 }
 
-type GetTemplateResponse struct {
-	Name string
+type GetAllResponse struct {
+	list []base
+}
+type base struct {
+	Name         string
+	HtmlTemplate string
 }
 
 type Templer interface {
 	Create(CreateRequest) (CreateResponse, error)
-	Get() (GetResponse, error)
-	GetTemplate(string) (GetTemplateResponse, error)
+	GetAll() (GetAllResponse, error)
+	Get(string) (GetResponse, error)
 }
 
 type TemplateService struct {
+	db db.Provider
 }
 
-func (t *TemplateService) Create(CreateRequest) (CreateResponse, error) {
-	return CreateResponse{}, nil
+func NewTemplateService(db_provider db.Provider) Templer {
+	return &TemplateService{
+		db: db_provider,
+	}
 }
 
-func (t TemplateService) Get() (GetResponse, error) {
-	return GetResponse{}, nil
+func (t *TemplateService) Create(req CreateRequest) (CreateResponse, error) {
+	isValid_Name := ValidateName(req.Name)
+	if !isValid_Name {
+		return CreateResponse{}, ErrSysInvalidName
+	}
+	isValid_HTML := ValidateHTML(req.HtmlTemplate)
+	if !isValid_HTML {
+		return CreateResponse{}, ErrSysInvalidTemplate
+	}
+	resp, err := t.db.Create(&db.CreateRequest{})
+	if err != nil {
+		switch err {
+		case db.ErrSysDuplicateName_L1:
+			return CreateResponse{}, ErrSysDuplicateName
+		}
+	}
+	return CreateResponse{
+		Name:    resp.Name,
+		Message: SUCCESS_MESSAGE,
+	}, nil
 }
 
-func (t TemplateService) GetTemplate(s string) (GetTemplateResponse, error) {
-	return GetTemplateResponse{}, nil
+func (t TemplateService) Get(r string) (GetResponse, error) {
+	rslt, err := t.db.Get(r)
+	if err != nil {
+		switch err {
+		case db.ErrSysUnknown_L1:
+			return GetResponse{}, nil
+		}
+	}
+	return GetResponse{
+		Name:         rslt.Name,
+		HtmlTemplate: rslt.HtmlTemplate,
+	}, nil
+}
+
+func (t TemplateService) GetAll() (GetAllResponse, error) {
+	rslt, err := t.db.GetAll()
+	if err != nil {
+		switch err {
+		case db.ErrSysUnknown_L1:
+			return GetAllResponse{}, db.ErrSysUnknown_L1
+		}
+	}
+	result := []base{}
+	for _, i := range rslt.List
+
+	return GetAllResponse{}, nil
 }
