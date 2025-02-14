5b058b0e02b1bcb291e2f490c9b6349efb378971
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-14 11:25:19 +0300

    + integration test complete

--- internal/core/domain/email/service/render/render.go
+++ internal/core/domain/email/service/render/render.go
@@ -28,17 +28,17 @@ type Renderer interface {
 }
 
 type RenderService struct {
-	templatService template.Templer
+	TemplateService template.Templer
 }
 
 func NewRenderService(tp template.Templer) Renderer {
 	return &RenderService{
-		templatService: tp,
+		TemplateService: tp,
 	}
 }
 
 func (s RenderService) Create(r *Request) (Response, error) {
-	queryResp, err := s.templatService.Get(r.Name)
+	queryResp, err := s.TemplateService.Get(r.Name)
 	if err != nil {
 		// switch err {
 		// case template.ErrSysUnknown:
