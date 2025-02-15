3ef82beed521608ebf4c9eacf44bb4c6e9064685
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-15 10:50:21 +0300

    + stored procedures

--- migration/email_template_db.sql
+++ migration/email_template_db.sql
@@ -8,4 +8,33 @@ CREATE TABLE IF NOT EXISTS public."templates"
     is_deleted BOOLEAN DEFAULT FALSE
 );
 
-COMMENT ON TABLE public."template" IS 'stores templates.';
+COMMENT ON TABLE public."templates" IS 'stores email templates.';
+
+-- procedures----------------------------
+CREATE OR REPLACE PROCEDURE writer(
+   name int,
+   html BYTEA
+)
+language plpgsql    
+as $$
+begin
+   
+   -- create teamplate
+    INSERT INTO public."templates"
+    Values(name, html)
+    commit;
+end;$$; 
+----------------------------------------
+CREATE OR REPLACE PROCEDURE reader(
+   name int
+)
+language plpgsql    
+as $$
+begin
+   
+   -- read teamplate
+    SELECT * FROM public."templates" as t
+    WHERE t.name = name 
+    commit;
+end;$$; 
+------------------------------------------
