86fc140c1ea831aa971a3de927e1cbdcc7aa2a3d
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-15 18:11:31 +0300

    + resources test complete

---
+++ internal/adapter/secondary/resource/db/mock.go
@@ -0,0 +1,42 @@
+package adapter
+
+import (
+	"context"
+
+	port "b2b.nati011.github.com/internal/port/resource"
+)
+
+type Mock struct {
+}
+
+func NewMock() port.DB {
+	return &Mock{}
+}
+
+func (p *Mock) GetByID(ctx *context.Context, id int) (port.GetResponse, error) {
+	return port.GetResponse{}, nil
+}
+
+func (p *Mock) GetByName(ctx *context.Context, name string) (port.GetResponse, error) {
+	return port.GetResponse{}, nil
+}
+
+func (p *Mock) GetAll(ctx *context.Context) (port.GetAllResponse, error) {
+	return port.GetAllResponse{}, nil
+}
+
+func (p *Mock) Create(ctx *context.Context, req *port.CreateRequest) (int, error) {
+	return 0, nil
+}
+
+func (p *Mock) UpdateAction(ctx *context.Context, req *port.UpdateActionRequest) (int, error) {
+	return 0, nil
+}
+
+func (p *Mock) UpdateName(ctx *context.Context, req *port.UpdateNameRequest) (int, error) {
+	return 0, nil
+}
+
+func (p *Mock) Delete(ctx *context.Context, id int) error {
+	return nil
+}
---
+++ internal/adapter/secondary/resource/db/pg.go
@@ -0,0 +1,42 @@
+package adapter
+
+import (
+	"context"
+
+	port "b2b.nati011.github.com/internal/port/resource"
+)
+
+type Postgres struct {
+}
+
+func NewPostgres() port.DB {
+	return &Postgres{}
+}
+
+func (p *Postgres) GetByID(ctx *context.Context, id int) (port.GetResponse, error) {
+	return port.GetResponse{}, nil
+}
+
+func (p *Postgres) GetByName(ctx *context.Context, name string) (port.GetResponse, error) {
+	return port.GetResponse{}, nil
+}
+
+func (p *Postgres) GetAll(ctx *context.Context) (port.GetAllResponse, error) {
+	return port.GetAllResponse{}, nil
+}
+
+func (p *Postgres) Create(ctx *context.Context, req *port.CreateRequest) (int, error) {
+	return 0, nil
+}
+
+func (p *Postgres) UpdateAction(ctx *context.Context, req *port.UpdateActionRequest) (int, error) {
+	return 0, nil
+}
+
+func (p *Postgres) UpdateName(ctx *context.Context, req *port.UpdateNameRequest) (int, error) {
+	return 0, nil
+}
+
+func (p *Postgres) Delete(ctx *context.Context, id int) error {
+	return nil
+}
