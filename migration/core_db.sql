35f3bbbdc7659b6b69f5e87931f1fb5770f99aa6
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-15 14:44:16 +0300

    + changed to relationship between role and resources to m2m

--- migration/core_db.sql
+++ migration/core_db.sql
@@ -35,12 +35,21 @@ CREATE TABLE IF NOT EXISTS public."resources"
 (
   id SERIAL PRIMARY KEY,
   action VARCHAR(4),
-  name VARCHAR(255),
-  role_id INT,
-  FOREIGN KEY (role_id) REFERENCES public."roles" (id) ON DELETE CASCADE
+  name VARCHAR(255)
 ) INHERITS (public."base");
 
 COMMENT ON TABLE public."resources" IS 'stores permissible resources for user agent role.';
+
+CREATE TABLE IF NOT EXISTS public."role_resources"
+(
+	role_id INT,
+	resource_id INT,
+	FOREIGN KEY (role_id) REFERENCES public."roles"(id) ON DELETE CASCADE,
+	FOREIGN KEY(resource_id) REFERENCES public."resources"(id) ON DELETE CASCADE
+) INHERITS (public."base");
+
+COMMENT ON TABLE public."role_resources" IS 'stores role resource mappings';
+
 
 CREATE TABLE IF NOT EXISTS public."retailers" 
 (
