ca83671023c884e1eba41bc186eb3c346bd67a54
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-17 18:39:25 +0300

    wired resource provider mock

--- internal/adapter/secondary/resource/db/pg.go
+++ internal/adapter/secondary/resource/db/pg.go
@@ -13,30 +13,30 @@ func NewPostgres() port.DB {
 	return &Postgres{}
 }
 
-func (p *Postgres) GetByID(ctx *context.Context, id int) (port.GetResponse, error) {
+func (p *Postgres) GetByID(ctx context.Context, id int) (port.GetResponse, error) {
 	return port.GetResponse{}, nil
 }
 
-func (p *Postgres) GetByName(ctx *context.Context, name string) (port.GetResponse, error) {
+func (p *Postgres) GetByName(ctx context.Context, name string) (port.GetResponse, error) {
 	return port.GetResponse{}, nil
 }
 
-func (p *Postgres) GetAll(ctx *context.Context) (port.GetAllResponse, error) {
+func (p *Postgres) GetAll(ctx context.Context) (port.GetAllResponse, error) {
 	return port.GetAllResponse{}, nil
 }
 
-func (p *Postgres) Create(ctx *context.Context, req *port.CreateRequest) (int, error) {
+func (p *Postgres) Create(ctx context.Context, req *port.CreateRequest) (int, error) {
 	return 0, nil
 }
 
-func (p *Postgres) UpdateAction(ctx *context.Context, req *port.UpdateActionRequest) (int, error) {
+func (p *Postgres) UpdateAction(ctx context.Context, req *port.UpdateActionRequest) (int, error) {
 	return 0, nil
 }
 
-func (p *Postgres) UpdateName(ctx *context.Context, req *port.UpdateNameRequest) (int, error) {
+func (p *Postgres) UpdateName(ctx context.Context, req *port.UpdateNameRequest) (int, error) {
 	return 0, nil
 }
 
-func (p *Postgres) Delete(ctx *context.Context, id int) error {
+func (p *Postgres) Delete(ctx context.Context, id int) error {
 	return nil
 }
