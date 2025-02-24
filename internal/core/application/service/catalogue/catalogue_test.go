48a7ff36f8da419db5170929a56c52215ecaa572
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-24 14:48:15 +0300

    + category, configurable_product, price, product, stock

---
+++ internal/core/application/service/catalogue/catalogue_test.go
@@ -0,0 +1,25 @@
+package catalogue
+
+import "testing"
+
+func Test_add_product(t *testing.T) {
+}
+
+func Test_add_configurable_product(t *testing.T) {
+}
+
+func Test_confirm(t *testing.T) {
+}
+
+func Test_get(t *testing.T) {
+	//all(configurable and normal)
+}
+
+func Test_update(t *testing.T) {
+}
+
+func Test_deactivate(t *testing.T) {
+}
+
+func Test_availability(t *testing.T) {
+}
