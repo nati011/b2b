4766f1427fb11d05d65ae2db554485e6c8acb187
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-26 16:44:18 +0300

    + failing tests for configurable product

--- internal/core/application/service/catalogue/configurable_product/configurable_product.go
+++ internal/core/application/service/catalogue/configurable_product/configurable_product.go
@@ -1,1 +1,312 @@
 package configurable_product
+
+import (
+	"context"
+	"errors"
+
+	port "b2b.nati011.github.com/internal/port/catalogue/configurable_product"
+)
+
+var (
+	ErrNameIsNotSupplied             = errors.New("oopsy, name is mandatory")
+	ErrNameDuplicate                 = errors.New("oopsy, name already in use")
+	ErrEmptyGetContent               = errors.New("oopsy, empty get content")
+	ErrUnknown                       = errors.New("oopsy, error unknown")
+	ErrDescIsNotSupplied             = errors.New("oopsy, desc is mandatory")
+	ErrImagesMustBeAtleastTwo        = errors.New("oopsy, atleast two images mandatory")
+	ErrAttributeKeysMustBeAtleastOne = errors.New("oopsy, atleast one attribute key mandatory")
+	ErrProductsMustBeAtleastOne      = errors.New("oopsy, atleast product mandatory")
+	ErrAlreadyAvailable              = errors.New("oopsy, product already available")
+	ErrAlreadyUnavailable            = errors.New("oopsy, product already unavailable")
+	ErrIdNotFound                    = errors.New("oopsy, id not found")
+)
+
+type CreateRequest struct {
+	Name          string
+	Desc          string
+	ExternalId    string
+	AttributeKeys []string
+	Products      []int
+	Images        []string
+}
+
+type PriceRangeResponse struct {
+	Min int
+	Max int
+}
+type GetResponse struct {
+	Id            int
+	Name          string
+	Desc          string
+	ExternalId    string
+	Attributes    map[string]string
+	Products      []int
+	IsAvailable   bool
+	PriceRange    PriceRangeResponse
+	CategoryId    []int
+	DistributorId int
+	Images        []string
+}
+
+type GetAllResponse struct {
+	List []GetResponse
+}
+
+type GetByParamRequest struct {
+	Name       string
+	ExternalId string
+}
+
+type UpdateRequest struct {
+}
+
+type Provider interface {
+	Create(ctx context.Context, req *CreateRequest) (int, error)
+	Avail(ctx context.Context, id int) error
+	Disable(ctx context.Context, id int) error
+	Get(ctx context.Context, id int) (GetResponse, error)
+	GetAll(ctx context.Context) (GetAllResponse, error)
+	GetByParam(ctx context.Context, req *GetByParamRequest) (GetAllResponse, error)
+	Update(ctx context.Context, req *UpdateRequest) error
+}
+
+type ConfigurableProductService struct {
+	db port.DB
+}
+
+func NewConfigurableProductService(db port.DB) Provider {
+	return &ConfigurableProductService{
+		db: db,
+	}
+}
+
+func (c *ConfigurableProductService) Create(ctx context.Context, req *CreateRequest) (int, error) {
+	err := c.validateName(ctx, req.Name)
+	if err != nil {
+		return 0, err
+	}
+	err = validateDesc(req.Desc)
+	if err != nil {
+		return 0, err
+	}
+	err = validateImages(req.Images)
+	if err != nil {
+		return 0, err
+	}
+
+	/*
+		validate products before validating attribute keys
+		because attribute validation relies on the products..
+		we fetch the attributes in our products and ensure that the
+		attributes exist
+	*/
+	err = c.validateProducts(ctx, req.Products)
+	if err != nil {
+		return 0, err
+	}
+	err = c.validateAttributekeys(ctx, req.AttributeKeys)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := c.db.Create(ctx, &port.CreateRequest{})
+	if err != nil {
+		switch err {
+		default:
+			return 0, ErrUnknown
+		}
+	}
+	return id, nil
+}
+
+func (c *ConfigurableProductService) Get(ctx context.Context, id int) (GetResponse, error) {
+	resp, err := c.db.Get(ctx, id)
+	if err != nil {
+		switch err {
+		case port.ErrSysNoRows:
+			return GetResponse{}, ErrIdNotFound
+		default:
+			return GetResponse{}, ErrUnknown
+		}
+	}
+
+	return GetResponse{
+		Id:            resp.Id,
+		Name:          resp.Name,
+		Desc:          resp.Desc,
+		ExternalId:    resp.ExternalId,
+		Products:      resp.Products,
+		IsAvailable:   resp.IsAvailable,
+		PriceRange:    PriceRangeResponse(resp.PriceRange),
+		CategoryId:    resp.CategoryId,
+		DistributorId: resp.DistributorId,
+		Images:        resp.Images,
+	}, nil
+}
+
+func (c *ConfigurableProductService) GetByParam(ctx context.Context, req *GetByParamRequest) (GetAllResponse, error) {
+	resp := []GetResponse{}
+	if req.Name != "" {
+		get_by_name_resp, err := c.db.GetByName(ctx, req.Name)
+		if err != nil {
+			switch err {
+			case port.ErrSysNoRows:
+			default:
+				return GetAllResponse{}, ErrUnknown
+			}
+		}
+		for _, i := range get_by_name_resp.List {
+			resp = append(resp, GetResponse{
+				Id:            i.Id,
+				Name:          i.Name,
+				Desc:          i.Desc,
+				ExternalId:    i.ExternalId,
+				Products:      i.Products,
+				IsAvailable:   i.IsAvailable,
+				PriceRange:    PriceRangeResponse(i.PriceRange),
+				CategoryId:    i.CategoryId,
+				DistributorId: i.DistributorId,
+				Images:        i.Images,
+			})
+		}
+	}
+
+	if req.ExternalId != "" {
+		get_by_name_resp, err := c.db.GetByExternalId(ctx, req.ExternalId)
+		if err != nil {
+			switch err {
+			case port.ErrSysNoRows:
+			default:
+				return GetAllResponse{}, ErrUnknown
+			}
+		}
+		for _, i := range get_by_name_resp.List {
+			resp = append(resp, GetResponse{
+				Id:            i.Id,
+				Name:          i.Name,
+				Desc:          i.Desc,
+				ExternalId:    i.ExternalId,
+				Products:      i.Products,
+				IsAvailable:   i.IsAvailable,
+				PriceRange:    PriceRangeResponse(i.PriceRange),
+				CategoryId:    i.CategoryId,
+				DistributorId: i.DistributorId,
+				Images:        i.Images,
+			})
+		}
+	}
+	if len(resp) == 0 {
+		return GetAllResponse{}, ErrEmptyGetContent
+	}
+	return GetAllResponse{
+		List: resp,
+	}, nil
+}
+
+func (c *ConfigurableProductService) GetAll(ctx context.Context) (GetAllResponse, error) {
+	resp := []GetResponse{}
+	get_by_name_resp, err := c.db.GetAll(ctx)
+	if err != nil {
+		switch err {
+		case port.ErrSysNoRows:
+		default:
+			return GetAllResponse{}, ErrUnknown
+		}
+	}
+	for _, i := range get_by_name_resp.List {
+		resp = append(resp, GetResponse{
+			Id:            i.Id,
+			Name:          i.Name,
+			Desc:          i.Desc,
+			ExternalId:    i.ExternalId,
+			Products:      i.Products,
+			IsAvailable:   i.IsAvailable,
+			PriceRange:    PriceRangeResponse(i.PriceRange),
+			CategoryId:    i.CategoryId,
+			DistributorId: i.DistributorId,
+			Images:        i.Images,
+		})
+	}
+	if len(resp) == 0 {
+		return GetAllResponse{}, ErrEmptyGetContent
+	}
+
+	return GetAllResponse{
+		List: resp,
+	}, nil
+}
+
+func (c *ConfigurableProductService) Avail(ctx context.Context, id int) error {
+	//validate
+	resp, err := c.db.Get(ctx, id)
+	if err != nil {
+		switch err {
+		case port.ErrSysNoRows:
+			return ErrIdNotFound
+		default:
+			return ErrUnknown
+		}
+	}
+
+	//check if present
+	if resp.IsAvailable {
+		return ErrAlreadyAvailable
+	}
+
+	err = c.db.UpdateIsAvailableStatus(ctx, &port.UpdateIsAvailableStatusRequest{
+		Id:     id,
+		Status: true,
+	})
+	if err != nil {
+		switch err {
+		default:
+			return ErrUnknown
+		}
+	}
+	return nil
+}
+
+func (c *ConfigurableProductService) Disable(ctx context.Context, id int) error {
+	//validate
+	resp, err := c.db.Get(ctx, id)
+	if err != nil {
+		switch err {
+		case port.ErrSysNoRows:
+			return ErrIdNotFound
+		default:
+			return ErrUnknown
+		}
+	}
+
+	//check if present
+	if !resp.IsAvailable {
+		return ErrAlreadyUnavailable
+	}
+
+	err = c.db.UpdateIsAvailableStatus(ctx, &port.UpdateIsAvailableStatusRequest{
+		Id:     id,
+		Status: true,
+	})
+	if err != nil {
+		switch err {
+		default:
+			return ErrUnknown
+		}
+	}
+	return nil
+}
+
+func (c *ConfigurableProductService) Update(ctx context.Context, req *UpdateRequest) error {
+	//validate
+	// resp, err := c.db.Get(ctx, req.)
+	// if err != nil {
+	// 	switch err {
+	// 	case port.ErrSysNoRows:
+	// 		return ErrIdNotFound
+	// 	default:
+	// 		return ErrUnknown
+	// 	}
+	// }
+
+	return nil
+}
--- internal/core/application/service/catalogue/configurable_product/configurable_product_test.go
+++ internal/core/application/service/catalogue/configurable_product/configurable_product_test.go
@@ -1,1 +1,453 @@
 package configurable_product
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	db "b2b.nati011.github.com/internal/adapter/secondary/catalogue/configurable_product"
+)
+
+var service Provider
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	os.Exit(code)
+}
+
+func setup() {
+	service = NewConfigurableProductService(
+		db.NewMock(),
+	)
+}
+func Test_Create_happyPath(t *testing.T) {
+	t.Run("create", func(t *testing.T) {
+		ctx := context.Background()
+		in := &CreateRequest{
+			Name:       "test",
+			Desc:       "test",
+			ExternalId: "test",
+			AttributeKeys: []string{
+				"test",
+				"test",
+			},
+			Products: []int{
+				1,
+			},
+			Images: []string{
+				"test",
+				"test",
+			},
+		}
+		id, err := service.Create(ctx, in)
+		if err != nil {
+			t.Fatalf("Failed to create err: %v", err)
+		}
+
+		got, err := service.Get(ctx, id)
+		if err != nil {
+			t.Fatalf("Failed to get err: %v", err)
+		}
+		if got.Id != id {
+			t.Errorf("Expected id: %v Got id: %v", id, got.Id)
+		}
+	})
+
+	t.Run("inactiveByDefault", func(t *testing.T) {
+		ctx := context.Background()
+		in := &CreateRequest{
+			Name:       "test",
+			Desc:       "test",
+			ExternalId: "test",
+			AttributeKeys: []string{
+				"test",
+				"test",
+			},
+			Products: []int{
+				1,
+			},
+			Images: []string{
+				"test",
+				"test",
+			},
+		}
+		id, err := service.Create(ctx, in)
+		if err != nil {
+			t.Fatalf("Failed to create err: %v", err)
+		}
+
+		got, err := service.Get(ctx, id)
+		if err != nil {
+			t.Fatalf("Failed to get err: %v", err)
+		}
+
+		wantIsAvailableStatus := false
+		if got.IsAvailable != wantIsAvailableStatus {
+			t.Errorf("Expected is available status: %v Got status: %v", wantIsAvailableStatus, got.IsAvailable)
+		}
+	})
+}
+
+func Test_Create_unhappyPath(t *testing.T) {
+	t.Run("name_mandatory", func(t *testing.T) {
+		ctx := context.Background()
+		in := &CreateRequest{
+			Desc:       "test",
+			ExternalId: "test",
+			AttributeKeys: []string{
+				"test",
+				"test",
+			},
+			Products: []int{
+				1,
+			},
+			Images: []string{
+				"test",
+				"test",
+			},
+		}
+		_, err := service.Create(ctx, in)
+		wantErr := ErrNameIsNotSupplied
+		if err != wantErr {
+			t.Fatalf("Expected err: %v Got err: %v", wantErr, err)
+		}
+	})
+
+	t.Run("duplicate_name", func(t *testing.T) {
+		ctx := context.Background()
+		in := &CreateRequest{
+			Name:       "test",
+			Desc:       "test",
+			ExternalId: "test",
+			AttributeKeys: []string{
+				"test",
+				"test",
+			},
+			Products: []int{
+				1,
+			},
+			Images: []string{
+				"test",
+				"test",
+			},
+		}
+		_, err := service.Create(ctx, in)
+		if err != nil {
+			t.Fatalf("Failed to create err: %v", err)
+		}
+
+		in_new := &CreateRequest{
+			Name:       "test",
+			Desc:       "test",
+			ExternalId: "test",
+			AttributeKeys: []string{
+				"test",
+				"test",
+			},
+			Products: []int{
+				1,
+			},
+			Images: []string{
+				"test",
+				"test",
+			},
+		}
+		_, err = service.Create(ctx, in_new)
+		wantErr := ErrNameDuplicate
+		if err != wantErr {
+			t.Errorf("Expected err: %v Got err: %v", wantErr, err)
+		}
+	})
+
+	t.Run("desc_mandatory", func(t *testing.T) {
+		ctx := context.Background()
+		in := &CreateRequest{
+			Name:       "test",
+			ExternalId: "test",
+			AttributeKeys: []string{
+				"test",
+				"test",
+			},
+			Products: []int{
+				1,
+			},
+			Images: []string{
+				"test",
+				"test",
+			},
+		}
+		_, err := service.Create(ctx, in)
+		wantErr := ErrDescIsNotSupplied
+		if err != wantErr {
+			t.Fatalf("Expected err: %v Got err: %v", wantErr, err)
+		}
+	})
+
+	t.Run("atleast_one_attributeKey_mandatory", func(t *testing.T) {
+		ctx := context.Background()
+		in := &CreateRequest{
+			Name:          "test",
+			Desc:          "test",
+			ExternalId:    "test",
+			AttributeKeys: []string{},
+			Products: []int{
+				1,
+			},
+			Images: []string{
+				"test",
+				"test",
+			},
+		}
+		_, err := service.Create(ctx, in)
+		wantErr := ErrAttributeKeysMustBeAtleastOne
+		if err != wantErr {
+			t.Fatalf("Expected err: %v Got err: %v", wantErr, err)
+		}
+	})
+
+	t.Run("atleast_one_product_mandatory", func(t *testing.T) {
+		ctx := context.Background()
+		in := &CreateRequest{
+			Name:       "test",
+			Desc:       "test",
+			ExternalId: "test",
+			AttributeKeys: []string{
+				"1",
+			},
+			Products: []int{},
+			Images: []string{
+				"test",
+				"test",
+			},
+		}
+		_, err := service.Create(ctx, in)
+		wantErr := ErrProductsMustBeAtleastOne
+		if err != wantErr {
+			t.Fatalf("Expected err: %v Got err: %v", wantErr, err)
+		}
+	})
+
+	t.Run("atleast_two_images_mandatory", func(t *testing.T) {
+		ctx := context.Background()
+		in := &CreateRequest{
+			Name:       "test",
+			Desc:       "test",
+			ExternalId: "test",
+			AttributeKeys: []string{
+				"1",
+			},
+			Products: []int{
+				1,
+			},
+			Images: []string{
+				"test",
+			},
+		}
+		_, err := service.Create(ctx, in)
+		wantErr := ErrImagesMustBeAtleastTwo
+		if err != wantErr {
+			t.Fatalf("Expected err: %v Got err: %v", wantErr, err)
+		}
+	})
+}
+
+func Test_Avail_happyPath(t *testing.T) {
+	//setup
+	ctx := context.Background()
+	in := &CreateRequest{
+		Name:       "test",
+		Desc:       "test",
+		ExternalId: "test",
+		AttributeKeys: []string{
+			"1",
+		},
+		Products: []int{
+			1,
+		},
+		Images: []string{
+			"test",
+			"test",
+		},
+	}
+	id, err := service.Create(ctx, in)
+	if err != nil {
+		t.Fatalf("Failed to create err: %v", err)
+	}
+
+	err = service.Avail(ctx, id)
+	if err != nil {
+		t.Errorf("Failed to avail err: %v", err)
+	}
+
+	expectedStatus := false
+	got, err := service.Get(ctx, id)
+	if err != nil {
+		t.Errorf("Expected status: %v Got status: %v", expectedStatus, got.IsAvailable)
+	}
+
+}
+
+func Test_Avail_unhappyPath(t *testing.T) {
+	t.Run("id_not_found", func(t *testing.T) {
+
+	})
+
+	t.Run("already_available", func(t *testing.T) {
+		// setup
+		ctx := context.Background()
+		in := &CreateRequest{
+			Name:       "test",
+			Desc:       "test",
+			ExternalId: "test",
+			AttributeKeys: []string{
+				"1",
+			},
+			Products: []int{
+				1,
+			},
+			Images: []string{
+				"test",
+			},
+		}
+		id, err := service.Create(ctx, in)
+		if err != nil {
+			t.Fatalf("Failed to create err: %v", err)
+		}
+
+		err = service.Avail(ctx, id)
+		if err != nil {
+			t.Errorf("Failed to avail err: %v", err)
+		}
+
+		err = service.Avail(ctx, id)
+		expectedErr := ErrAlreadyAvailable
+		if err != expectedErr {
+			t.Errorf("Expected err: %v Got err: %v", expectedErr, err)
+		}
+	})
+}
+
+func Test_Disable_happyPath(t *testing.T) {
+	// setup
+	ctx := context.Background()
+	in := &CreateRequest{
+		Name:       "test",
+		Desc:       "test",
+		ExternalId: "test",
+		AttributeKeys: []string{
+			"1",
+		},
+		Products: []int{
+			1,
+		},
+		Images: []string{
+			"test",
+			"test",
+		},
+	}
+	id, err := service.Create(ctx, in)
+	if err != nil {
+		t.Fatalf("Failed to create err: %v", err)
+	}
+
+	err = service.Avail(ctx, id)
+	if err != nil {
+		t.Errorf("Failed to avail err: %v", err)
+	}
+
+	err = service.Disable(ctx, id)
+	if err != nil {
+		t.Fatalf("Failed to disavail err: %v", err)
+	}
+
+	got, err := service.Get(ctx, id)
+	if err != nil {
+		t.Fatalf("Failed to get err: %v", err)
+	}
+
+	wantStatus := false
+	if got.IsAvailable != wantStatus {
+		t.Errorf("Expected status: %v Got status: %v", wantStatus, got.IsAvailable)
+	}
+}
+
+func Test_Disable_unhappyPath(t *testing.T) {
+
+	t.Run("id_not_found", func(t *testing.T) {
+
+	})
+
+	t.Run("already_disabled", func(t *testing.T) {
+		// setup
+		ctx := context.Background()
+		in := &CreateRequest{
+			Name:       "test",
+			Desc:       "test",
+			ExternalId: "test",
+			AttributeKeys: []string{
+				"1",
+			},
+			Products: []int{
+				1,
+			},
+			Images: []string{
+				"test",
+				"test",
+			},
+		}
+		id, err := service.Create(ctx, in)
+		if err != nil {
+			t.Fatalf("Failed to create err: %v", err)
+		}
+
+		err = service.Disable(ctx, id)
+		expectedErr := ErrAlreadyUnavailable
+		if err != expectedErr {
+			t.Errorf("Expected err: %v Got err: %v", expectedErr, err)
+		}
+	})
+}
+
+func Test_Get(t *testing.T) {
+	//setup
+	ctx := context.Background()
+	in := &CreateRequest{
+		Name:       "test",
+		Desc:       "test",
+		ExternalId: "test",
+		AttributeKeys: []string{
+			"test",
+			"test",
+		},
+		Products: []int{
+			1,
+		},
+		Images: []string{
+			"test",
+			"test",
+		},
+	}
+	id, err := service.Create(ctx, in)
+	if err != nil {
+		t.Fatalf("Failed to create err: %v", err)
+	}
+
+	//Get
+	got, err := service.Get(ctx, id)
+	if err != nil {
+		t.Fatalf("Failed to Get err: %v", err)
+	}
+
+	if got.Id != id {
+		t.Errorf("Expected resp Id: %v Got: %v", id, got.Id)
+	}
+}
+
+func Test_GetByParam(t *testing.T) {
+
+}
+
+func Test_GetAll(t *testing.T) {
+
+}
