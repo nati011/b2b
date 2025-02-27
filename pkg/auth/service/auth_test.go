25cdfa7c04cc64ba224c5715f80b7ca49e8c45eb
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-27 10:37:08 +0300

    + fix remining issues

--- pkg/auth/service/auth_test.go
+++ pkg/auth/service/auth_test.go
@@ -3,8 +3,8 @@ package auth
 import (
 	"testing"
 
-	"b2b.nati011.github.com/internal/core/application/util/auth/provider"
 	authDTO "b2b.nati011.github.com/pkg/auth/model/dto"
+	"b2b.nati011.github.com/pkg/auth/provider"
 )
 
 const (
