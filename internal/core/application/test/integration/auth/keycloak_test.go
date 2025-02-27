8b7c91075e67103e49a6f3d90ae884a09653e965
natnael jemaneh <njemaneh@chegebeya.com>
2025-02-27 10:33:39 +0300

    + fixed pkg name issues caused by the refactoring effort

--- internal/core/application/test/integration/auth/keycloak_test.go
+++ internal/core/application/test/integration/auth/keycloak_test.go
@@ -5,9 +5,9 @@ import (
 	"os"
 	"testing"
 
-	authDTO "b2b.nati011.github.com/internal/core/application/util/auth/model/dto"
-	provider "b2b.nati011.github.com/internal/core/application/util/auth/provider"
-	service "b2b.nati011.github.com/internal/core/application/util/auth/service"
+	authDTO "b2b.nati011.github.com/pkg/auth/model/dto"
+	provider "b2b.nati011.github.com/pkg/auth/provider"
+	service "b2b.nati011.github.com/pkg/auth/service"
 
 	keycloak "github.com/stillya/testcontainers-keycloak"
 )
