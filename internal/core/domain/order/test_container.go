832aab1135b6d399ccce7d21028a25b7448c0ce6
natnael jemaneh <njemaneh@chegebeya.com>
2025-03-01 13:52:29 +0300

    + resolve package issues

--- internal/core/domain/order/test_container.go
+++ internal/core/domain/order/test_container.go
@@ -1,8 +1,8 @@
 package order
 
 import (
-	invoice_db "b2b.nati011.github.com/internal/adapter/secondary/invoice"
-	order_db "b2b.nati011.github.com/internal/adapter/secondary/order"
+	invoice_db "b2b.nati011.github.com/internal/adapter/secondary/domain/invoice"
+	order_db "b2b.nati011.github.com/internal/adapter/secondary/domain/order"
 	"b2b.nati011.github.com/internal/core/domain/invoice"
 )
 
