237491f958c4c1c0cfe1e2f4df4a4b9899080bd8
natnael jemaneh <njemaneh@chegebeya.com>
2025-03-03 14:41:19 +0300

    - remove service dir

--- internal/core/application/payment/payment_test.go
+++ internal/core/application/payment/payment_test.go
@@ -14,41 +14,8 @@ func TestMain(m *testing.M) {
 func setup() {
 }
 
-func Test_Create_Payment_Option(t *testing.T) {
+func Test_Init(t *testing.T) {
 }
 
-func Test_Activate_Payment_Option(t *testing.T) {
-}
-
-func Test_Deactivate_Payment_Option(t *testing.T) {
-}
-
-func Test_Get_All_Payment_Options(t *testing.T) {
-}
-
-func Test_Get_Active_Payment_Options(t *testing.T) {
-}
-
-func Test_Checkout(t *testing.T) {
-}
-
-func Test_Payment_Confirmation(t *testing.T) {
-}
-
-func Test_Get_Payment_Status(t *testing.T) {
-}
-
-func Test_Save_Transaction(t *testing.T) {
-}
-
-func Test_Get_Transaction_History(t *testing.T) {
-}
-
-func Test_Get_Transaction_History_By_User(t *testing.T) {
-}
-
-func Test_Get_Transaction_History_By_Status(t *testing.T) {
-}
-
-func Test_GetAll_Transaction_History(t *testing.T) {
+func Test_Cancel(t *testing.T) {
 }
