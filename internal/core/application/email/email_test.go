073eb314293787653278e27a5fa1e9efd22141dc
natnael jemaneh <njemaneh@chegebeya.com>
2025-03-06 16:55:30 +0300

    + separate email packages

--- internal/core/application/email/email_test.go
+++ internal/core/application/email/email_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	smtp "b2b.nati011.github.com/internal/adapter/secondary/application/email/smtp"
-	render "b2b.nati011.github.com/internal/core/application/email/render"
+	render "b2b.nati011.github.com/internal/core/application/render"
 )
 
 const (
