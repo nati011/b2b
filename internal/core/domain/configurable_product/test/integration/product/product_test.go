1c01f11837be539d94db7d9894e494cbfb75d788
natnael jemaneh <njemaneh@chegebeya.com>
2025-03-06 17:58:21 +0300

    + auto populate configurable product attribute keys upon configurable product create op

--- internal/core/domain/configurable_product/test/integration/product/product_test.go
+++ internal/core/domain/configurable_product/test/integration/product/product_test.go
@@ -139,7 +139,7 @@ func Test_Create_ValidateAttribute_keys_happyPath(t *testing.T) {
 	}
 }
 
-func Test_Create_PopulateAttributeValues(t *testing.T) {
+func Test_Create_ValidateAttribute_keys_unhappyPath(t *testing.T) {
 	//create product
 	ctx := context.Background()
 	in := &product.CreateRequest{
@@ -182,6 +182,66 @@ func Test_Create_PopulateAttributeValues(t *testing.T) {
 	if err != wantErr {
 		t.Errorf("Expected err: %v, Got err: %v", wantErr, err)
 	}
+}
+
+func Test_Create_PopulateAttributeValues_happyPath(t *testing.T) {
+	//create product
+	ctx := context.Background()
+	in := &product.CreateRequest{
+		Name:       "test",
+		Desc:       "test",
+		ExternalID: "123",
+		Images: []string{
+			"test",
+			"test",
+		},
+		Price: 100.00,
+		Attributes: map[string]string{
+			"test": "test",
+		},
+	}
+
+	id, err := productService.Create(ctx, in)
+	if err != nil {
+		t.Errorf("Failed to create product err: %v", err)
+	}
+
+	//create configurable product
+	in_cp := &configurable_product.CreateRequest{
+		Name:       "test",
+		Desc:       "test",
+		ExternalId: "test",
+		AttributeKeys: []string{
+			"test",
+		},
+		Products: []int{
+			id,
+		},
+		Images: []string{
+			"test",
+			"test",
+		},
+	}
+	cp_id, err := configurableProductService.Create(ctx, in_cp)
+	if err != nil {
+		t.Fatalf("Failed to create config prod %v", err)
+	}
+	wantAttributes := map[string]string{
+		"test": "test",
+	}
+	resp, err := configurableProductService.Get(ctx, cp_id)
+	if err != nil {
+		t.Fatalf("Failed to fetch product err: %v", err)
+	}
+	for _, i := range resp.Attributes {
+		if i != wantAttributes["test"] {
+			t.Errorf("Failed to populate attributes")
+		}
+	}
+}
+
+func Test_Create_PopulateAttributeValues_unhappyPath(t *testing.T) {
+
 }
 
 func Test_Update_ValidateProduct_happyPath(t *testing.T) {
