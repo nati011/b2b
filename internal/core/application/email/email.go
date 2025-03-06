4bbf88e2b77a61bca4d2e8584ed0b27f6e3d63c3
natnael jemaneh <njemaneh@chegebeya.com>
2025-03-06 16:55:30 +0300

    + separate email packages

--- internal/core/application/email/email.go
+++ internal/core/application/email/email.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 
 	smtp "b2b.nati011.github.com/internal/adapter/secondary/application/email/smtp"
-	render "b2b.nati011.github.com/internal/core/application/email/render"
+	render "b2b.nati011.github.com/internal/core/application/render"
 )
 
 type SendRequest struct {
