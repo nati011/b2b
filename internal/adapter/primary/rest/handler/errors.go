76d6e4545b94240cddcc9acad506d39914178f0a
natnael jemaneh <njemaneh@chegebeya.com>
2025-03-07 18:18:23 +0300

    + add handler

---
+++ internal/adapter/primary/rest/handler/errors.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+var logger = log.New(os.Stdout, "api: ", log.LstdFlags)
+
+func logError(r *http.Request, err error) {
+	logger.Println(err)
+}
+
+func errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
+	env := Envelope{"error": message}
+
+	err := WriteJSON(w, env, nil)
+	if err != nil {
+		logError(r, err)
+		w.WriteHeader(500)
+	}
+}
+
+func ServerErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	logError(r, err)
+	message := "the server encountered a problem and could not process your request"
+	errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+func RequestErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	logError(r, err)
+	message := "the server encountered a problem and could not process your request"
+	errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+func NotFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the requested resource could not be found"
+	errorResponse(w, r, http.StatusNotFound, message)
+}
