f8167a4a31bb465dfea3b20945cfc7fe86053cbf
natnael jemaneh <njemaneh@chegebeya.com>
2025-03-10 13:51:20 +0300

    + wired up server

--- cmd/init_rest.go
+++ cmd/init_rest.go
@@ -1,6 +1,22 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+	"net/http"
 
-func InitREST(db *sql.DB) {
+	"b2b.nati011.github.com/internal/adapter/primary/rest"
+	"b2b.nati011.github.com/internal/core"
+)
+
+var (
+	ErrFailedToBuildRoutes = errors.New("oopsy, failed to build routes")
+)
+
+func InitREST(mux *http.ServeMux, db *sql.DB, services *core.MasterContainer) *http.ServeMux {
+	err := rest.BuildRouter(mux, services)
+	if err != nil {
+		panic(ErrFailedToBuildRoutes)
+	}
+	return mux
 }
--- internal/adapter/primary/rest/router.go
+++ internal/adapter/primary/rest/router.go
@@ -4,9 +4,10 @@ import (
 	"net/http"
 
 	"b2b.nati011.github.com/internal/adapter/primary/rest/handler"
+	"b2b.nati011.github.com/internal/core"
 )
 
-func BuildRouter(mux *http.ServeMux) error {
+func BuildRouter(mux *http.ServeMux, services *core.MasterContainer) error {
 	for _, h := range handler.GetHandlers() {
 		if err := h.Init(); err != nil {
 			return err
