8c36cc8b2493d464e9fead4b8ed1c310ed819aa0
expired-pineapple <ruthtirusew944@gmail.com>
2025-03-10 17:01:06 +0300

    inprogress: merge business and distributor modules

--- internal/adapter/secondary/application/distributor/db/pg.go
+++ internal/adapter/secondary/application/distributor/db/pg.go
@@ -74,6 +74,50 @@ func (p *Postgres) GetById(ctx context.Context, id int) (port.GetResponse, error
 	return response, nil
 }
 
+func (p *Postgres) Update(ctx context.Context, req *port.CreateBusinessInformation) (port.CreateBusinessResponse, error) {
+	panic("unimplemented")
+}
+
+func (p *Postgres) CreateBusinessInformation(ctx context.Context, req *port.CreateBusinessInformation) (port.CreateBusinessResponse, error) {
+	var resourceId int
+	var resp port.CreateBusinessResponse
+	query := "SELECT * FROM public.create_distributor_business_location($1, $2, $3, $4, $5, $6);"
+
+	err := p.db.QueryRowContext(ctx, query,
+		req.Name,
+		req.Tin,
+		req.DistributorId,
+		req.GeneralZone,
+		req.Region,
+		req.Woreda,
+	).Scan(&resourceId)
+
+	if err != nil {
+		print(err.Error())
+		switch err {
+		case sql.ErrNoRows:
+			return resp, port.ErrSysNoRows
+		default:
+			return resp, port.ErrSysUnknown
+		}
+	}
+	resp = port.CreateBusinessResponse{
+		BusinessId: resourceId,
+	}
+	return resp, nil
+}
+
+func (p *Postgres) GetBusinessAll(ctx context.Context) (port.GetAllResponse, error) {
+	panic("Unimplemented")
+}
+
+func (p *Postgres) GetBusinessById(ctx context.Context, id int) (port.GetResponse, error) {
+	panic("Unimplemented")
+}
+func (p *Postgres) GetByDistributorId(ctx context.Context, id int) (port.GetResponse, error) {
+	panic("Unimplemented")
+}
+
 func NewPostgres(db *sql.DB) port.DB {
 	return &Postgres{
 		db: db,
