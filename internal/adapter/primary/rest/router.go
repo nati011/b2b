6a12722b1a891b1c53ae7d7c9987447bdf0a511f
natnael jemaneh <njemaneh@chegebeya.com>
2025-03-18 09:17:32 +0300

    + cleanup

--- internal/adapter/primary/rest/router.go
+++ internal/adapter/primary/rest/router.go
@@ -7,13 +7,14 @@ import (
 	application_handler "b2b.nati011.github.com/internal/adapter/primary/rest/handler/application"
 	application_core "b2b.nati011.github.com/internal/core/application"
 
-	// domain_handler "b2b.nati011.github.com/internal/adapter/primary/rest/handler/domain"
+	domain_handler "b2b.nati011.github.com/internal/adapter/primary/rest/handler/domain"
 	domain_core "b2b.nati011.github.com/internal/core/domain"
 )
 
 func BuildRouter(mux *http.ServeMux, applicationServices *application_core.Container, domainServices *domain_core.Container) error {
 
 	application_handler.InitResource()
+	domain_handler.InitProduct()
 
 	for _, h := range handler.GetHandlers() {
 		if err := h.Init(applicationServices, domainServices); err != nil {
