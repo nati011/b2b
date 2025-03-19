a79a0a09784a0919b0865ed7b8f21b5af4396950
natnael jemaneh <njemaneh@chegebeya.com>
2025-03-19 17:04:18 +0300

    + get context from request

--- internal/adapter/primary/rest/handler/application/auth.go
+++ internal/adapter/primary/rest/handler/application/auth.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"context"
 	"encoding/json"
 	"net/http"
 
@@ -33,14 +32,13 @@ func (a *AuthHandler) Routes(mux *http.ServeMux) {
 }
 
 func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
 	var req port.LoginUserRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
 
-	loginResponse, err := h.service.ClientLogin(ctx, req)
+	loginResponse, err := h.service.ClientLogin(r.Context(), req)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusUnauthorized)
@@ -51,7 +49,6 @@ func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *AuthHandler) RefreshToken(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
@@ -64,7 +61,7 @@ func (h *AuthHandler) RefreshToken(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	refreshResponse, err := h.service.RefreshToken(ctx, req)
+	refreshResponse, err := h.service.RefreshToken(r.Context(), req)
 	if err != nil {
 		http.Error(w, "Authentication failed", http.StatusUnauthorized)
 		return
