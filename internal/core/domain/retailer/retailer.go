023d9085e29604c49c3fcaa7d001b75904a5abee
natnael jemaneh <njemaneh@chegebeya.com>
2025-03-22 17:33:21 +0300

    resolve conflict

--- internal/core/domain/retailer/retailer.go
+++ internal/core/domain/retailer/retailer.go
@@ -4,189 +4,156 @@ import (
 	"context"
 	"errors"
 
-	authPort "b2b.nati011.github.com/internal/port/application/auth/provider"
+	"b2b.nati011.github.com/internal/core/application/user"
 	port "b2b.nati011.github.com/internal/port/application/retailer"
-
-	"b2b.nati011.github.com/internal/core/application/auth"
 )
 
 var (
-	ErrEmptyGetRetailerContent = errors.New("oopsy, no retailer found")
-	ErrEmptyGetBusinessContent = errors.New("oopsy, no business found")
-	ErrUnknown                 = errors.New("oopsy, unkown error")
-	SUCCESS_MESSAGE            = "Ahoy!"
+	ErrUnknown         = errors.New("oopsy, unknown error")
+	ErrInvalidTin      = errors.New("oopsy, tin invalid")
+	ErrInvalidLatitude = errors.New("oopsy, latitude invalid")
+	ErrIdNotFound      = errors.New("oopsy, id not found")
+	ErrEmptyGetContent = errors.New("oopsy, empty get content")
 )
 
+type CreateRequest struct {
+	Tin         string
+	Latitude    string
+	Longitude   string
+	GeneralZone string
+	Region      string
+	Woreda      string
+
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	UserId    int
+}
+
+type GetResponse struct {
+	Id          int
+	Tin         string
+	Latitude    string
+	Longitude   string
+	GeneralZone string
+	Region      string
+	Woreda      string
+}
+
+type GetAllResponse struct {
+	List []GetResponse
+}
+
+type GetByParamRequest struct {
+	Name string
+	Tin  string
+}
+
+type UpdateRequest struct {
+	Id   int
+	Name string
+	Tin  string
+}
+
 type Provider interface {
-	Create(ctx context.Context, req *port.RegisterRetailerRequest) (response port.RegisterRetailerResponse, err error)
-	GetAll(ctx context.Context) (port.GetAllResponse, error)
-	GetByParam(ctx context.Context, req *port.GetByParamRequest) (port.GetResponse, error)
-	AddBusinessInformattion(ctx context.Context, req *port.CreateBusinessInformation) (response port.CreateBusinessResponse, err error)
-	GetBusinessAll(ctx context.Context) (port.GetAllResponse, error)
-	GetByRetailer(ctx context.Context, retailerId int) (port.GetBusinessResponse, error)
-	GetById(ctx context.Context, id int) (port.GetResponse, error)
-	UpdateBusiness(ctx context.Context, req *port.UpdateBusinessRequest) (response port.RegisterRetailerResponse, err error)
+	Create(ctx context.Context, req *CreateRequest) (int, error)
+	Get(ctx context.Context, id int) (GetResponse, error)
+	GetByParam(ctx context.Context, req *GetByParamRequest) (GetAllResponse, error)
+	Update(ctx context.Context, req *UpdateRequest) error
 }
 
 type RetailerService struct {
-	db          port.DB
-	authService auth.Provider
+	DB          port.DB
+	UserService user.Provider
 }
 
-func (d *RetailerService) Create(ctx context.Context, req *port.RegisterRetailerRequest) (response port.RegisterRetailerResponse, err error) {
-	resp := port.RegisterRetailerResponse{}
-	distribtor := port.CreateRequest{
-		FirstName:  req.FirstName,
-		Email:      req.Email,
-		DOB:        req.DOB,
-		Username:   req.Username,
-		ExternalId: req.ExternalId,
-	}
-
-	id, err := d.db.Create(ctx, &distribtor)
-	if err != nil {
-
-		return resp, err
-
-	}
-
-	user := authPort.RegisterUserRequest{
-		FirstName:   req.FirstName,
-		Email:       req.Email,
-		BirthDate:   req.DOB,
-		PhoneNumber: req.PhoneNumber,
-		Username:    req.Username,
-		ExternalId:  req.ExternalId,
-	}
-	_, err = d.authService.CreateNewClient(ctx, user)
-	if err != nil {
-		return resp, err
-
-	}
-
-	resp = port.RegisterRetailerResponse{
-		Message: SUCCESS_MESSAGE,
-		Id:      id,
-	}
-	return resp, nil
-}
-
-func (d *RetailerService) GetAll(ctx context.Context) (port.GetAllResponse, error) {
-	resp, err := d.db.GetAll(ctx)
-	if err != nil {
-		switch err {
-		case port.ErrSysNoRows:
-			return port.GetAllResponse{}, ErrEmptyGetRetailerContent
-		default:
-			return port.GetAllResponse{}, ErrUnknown
-		}
-	}
-
-	resp_val := port.GetAllResponse{}
-	for _, i := range resp.List {
-		resp_val.List = append(resp_val.List, port.GetResponse(i))
-	}
-	return resp_val, nil
-}
-
-func (d *RetailerService) GetByParam(ctx context.Context, req *port.GetByParamRequest) (port.GetResponse, error) {
-	resp, err := d.db.GetById(ctx, req.Id)
-	if err != nil {
-		switch err {
-		case port.ErrSysNoRows:
-			return port.GetResponse{}, ErrEmptyGetRetailerContent
-		default:
-			return port.GetResponse{}, ErrUnknown
-		}
-	}
-
-	resp_val := port.GetResponse{
-		Id:        resp.Id,
-		Email:     resp.Email,
-		FirstName: resp.FirstName,
-		LastName:  resp.LastName,
-	}
-
-	return resp_val, nil
-}
-func (d *RetailerService) AddBusinessInformattion(ctx context.Context, req *port.CreateBusinessInformation) (resp port.CreateBusinessResponse, err error) {
-	resp, err = d.db.CreateBusiness(ctx, req)
-	if err != nil {
-		return resp, err
-	}
-	return resp, nil
-}
-
-func (d *RetailerService) GetBusinessAll(ctx context.Context) (resp port.GetAllResponse, err error) {
-	resp, err = d.db.GetAll(ctx)
-	if err != nil {
-		switch err {
-		case port.ErrSysNoRows:
-			return resp, ErrEmptyGetBusinessContent
-		default:
-			return resp, ErrUnknown
-		}
-	}
-	return resp, nil
-}
-
-func (d *RetailerService) GetByRetailer(ctx context.Context, retailerId int) (resp port.GetBusinessResponse, err error) {
-	resp, err = d.db.GetByRetailerId(ctx, retailerId)
-	if err != nil {
-		switch err {
-		case port.ErrSysNoRows:
-			return resp, ErrEmptyGetRetailerContent
-		default:
-			return resp, ErrUnknown
-		}
-	}
-	return resp, nil
-}
-func (d *RetailerService) GetById(ctx context.Context, id int) (resp port.GetResponse, err error) {
-	resp, err = d.db.GetById(ctx, id)
-	if err != nil {
-		switch err {
-		case port.ErrSysNoRows:
-			return resp, ErrEmptyGetRetailerContent
-		default:
-			return resp, ErrUnknown
-		}
-	}
-	return resp, nil
-}
-
-func (d *RetailerService) UpdateBusiness(ctx context.Context, req *port.UpdateBusinessRequest) (response port.RegisterRetailerResponse, err error) {
-	_, err = d.db.GetBusinessById(
-		ctx,
-		req.Id,
-	)
-
-	if err != nil {
-		switch err {
-		case port.ErrSysNoRows:
-			return response, ErrEmptyGetRetailerContent
-
-		default:
-			return response, ErrUnknown
-		}
-	}
-
-	id, err := d.db.UpdateBusiness(ctx, req)
-	if err != nil {
-
-		return response, err
-
-	}
-
-	response = port.RegisterRetailerResponse{
-		Message: SUCCESS_MESSAGE,
-		Id:      id,
-	}
-	return response, nil
-}
-func NewRetailerService(db port.DB, authService auth.Provider) Provider {
+func NewRetailerService(up user.Provider, db port.DB) Provider {
 	return &RetailerService{
-		db:          db,
-		authService: authService,
+		UserService: up,
+		DB:          db,
 	}
+}
+
+func (r *RetailerService) Create(ctx context.Context, req *CreateRequest) (int, error) {
+	//validate
+	err := validateLat(req.Latitude)
+	if err != nil {
+		return 0, err
+	}
+
+	err = validateLong(req.Longitude)
+	if err != nil {
+		return 0, err
+	}
+
+	err = validateTin(req.Tin)
+	if err != nil {
+		return 0, err
+	}
+
+	// create user
+	_, err = r.UserService.Create(ctx, &user.CreateRequest{
+		FirstName: req.FirstName,
+		LastName:  req.LastName,
+		Email:     req.Email,
+		Phone:     req.Phone,
+	})
+	if err != nil {
+		switch err {
+		case user.ErrEmailNotValid, user.ErrPhoneNotValid, user.ErrPhoneOrEmailMandatory, user.ErrFirstNameMandatory:
+			return 0, err
+		default:
+			return 0, ErrUnknown
+		}
+	}
+
+	// create retailer
+	id, err := r.DB.Create(ctx, port.CreateRequest{
+		Name:        req.FirstName + req.LastName,
+		Tin:         req.Tin,
+		Latitude:    req.Latitude,
+		Longitude:   req.Longitude,
+		GeneralZone: req.GeneralZone,
+		Region:      req.Region,
+		Woreda:      req.Woreda,
+		UserId:      req.UserId,
+	})
+	if err != nil {
+		switch err {
+		default:
+			return 0, ErrUnknown
+		}
+	}
+	return id, nil
+}
+
+func (r *RetailerService) Get(ctx context.Context, id int) (GetResponse, error) {
+	resp, err := r.DB.Get(ctx, id)
+	if err != nil {
+		switch err {
+		case port.ErrSysNoRows:
+			return GetResponse{}, ErrIdNotFound
+		default:
+			return GetResponse{}, ErrUnknown
+		}
+	}
+
+	return GetResponse{
+		Id:          resp.Id,
+		Tin:         resp.Tin,
+		Latitude:    resp.Latitude,
+		Longitude:   resp.Longitude,
+		GeneralZone: resp.GeneralZone,
+		Region:      resp.Region,
+		Woreda:      resp.Woreda,
+	}, nil
+}
+
+func (r *RetailerService) GetByParam(ctx context.Context, req *GetByParamRequest) (GetAllResponse, error) {
+	return GetAllResponse{}, nil
+}
+
+func (r *RetailerService) Update(ctx context.Context, req *UpdateRequest) error {
+	return nil
 }
