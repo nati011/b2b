9bd746d823b7d65f546a129055c3cdc7a465d9f6
natnael jemaneh <njemaneh@chegebeya.com>
2025-03-31 17:49:24 +0300

    + refactor port: place distributor and retailer in domain

--- internal/adapter/secondary/application/distributor/db/mock.go
+++ internal/adapter/secondary/application/distributor/db/mock.go
@@ -3,7 +3,7 @@ package db
 import (
 	"context"
 
-	port "b2b.nati011.github.com/internal/port/application/distributor"
+	port "b2b.nati011.github.com/internal/port/domain/distributor"
 )
 
 type MockDistributor struct {
