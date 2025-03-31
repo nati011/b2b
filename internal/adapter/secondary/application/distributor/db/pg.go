61827cde49fcdbf1f3235fbe167edb21d1ebfaad
natnael jemaneh <njemaneh@chegebeya.com>
2025-03-31 16:19:21 +0300

    + implement distributor adapter

--- internal/adapter/secondary/application/distributor/db/pg.go
+++ internal/adapter/secondary/application/distributor/db/pg.go
@@ -2,30 +2,36 @@ package db
 
 import (
 	"context"
-
 	"database/sql"
+	"log"
+	"strings"
 
 	port "b2b.nati011.github.com/internal/port/application/distributor"
 )
 
 type Postgres struct {
-	db *sql.DB
+	Pool *sql.DB
 }
 
-func (p *Postgres) Create(ctx context.Context, req *port.CreateRequest) (int, error) {
-	var resourceId int
-	query := "SELECT * FROM public.create_distributor_user($1, $2, $3, $4, $5, $6, $7);"
+func NewPostgres(db *sql.DB) port.DB {
+	return &Postgres{
+		Pool: db,
+	}
+}
 
-	err := p.db.QueryRowContext(ctx, query,
-		req.FirstName,
-		req.LastName,
-		req.Email,
-		req.Phone,
-		req.Username,
-		req.DOB,
-		req.ExternalId,
-	).Scan(&resourceId)
+func (r *Postgres) Create(ctx context.Context, req port.CreateRequest) (int, error) {
+	var retailerId int
+	query := "SELECT * FROM public.create_distributor($1, $2, $3, $4, $5, $6, $7);"
 
+	err := r.Pool.QueryRowContext(ctx, query,
+		req.Name,
+		req.Tin,
+		req.Latitude,
+		req.Longitude,
+		req.GeneralZone,
+		req.Region,
+		req.Woreda,
+	).Scan(&retailerId)
 	if err != nil {
 		switch err {
 		case sql.ErrNoRows:
@@ -34,34 +40,39 @@ func (p *Postgres) Create(ctx context.Context, req *port.CreateRequest) (int, er
 			return 0, port.ErrSysUnknown
 		}
 	}
-
-	return resourceId, nil
-}
-
-func (p *Postgres) GetAll(ctx context.Context) (port.GetAllResponse, error) {
-	var response port.GetAllResponse
-
-	query := "SELECT * FROM public.get_all_distributors();"
-
-	err := p.db.QueryRowContext(ctx, query).Scan(response)
+	err = r.CreateRetailerUser(ctx, &port.CreateUserAgentRequest{
+		User_id:     req.UserId,
+		Retailer_id: retailerId,
+	})
 	if err != nil {
 		switch err {
-		case sql.ErrNoRows:
-			return port.GetAllResponse{}, port.ErrSysNoRows
 		default:
-			return port.GetAllResponse{}, port.ErrSysUnknown
+			return 0, port.ErrSysUnknown
 		}
 	}
 
-	return response, nil
+	return retailerId, nil
 }
 
-func (p *Postgres) GetById(ctx context.Context, id int) (port.GetResponse, error) {
+func (r *Postgres) CreateRetailerUser(ctx context.Context, req *port.CreateUserAgentRequest) error {
+	query := "SELECT * FROM public.create_distributor_user($1, $2);"
+
+	_, err := r.Pool.QueryContext(ctx, query, req.Retailer_id, req.User_id)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return port.ErrSysNoRows
+		default:
+			return port.ErrSysUnknown
+		}
+	}
+	return nil
+}
+
+func (r *Postgres) Get(ctx context.Context, id int) (port.GetResponse, error) {
 	var response port.GetResponse
-
 	query := "SELECT * FROM public.get_distributor_by_id($1);"
-
-	err := p.db.QueryRowContext(ctx, query, id).Scan(&response)
+	err := r.Pool.QueryRowContext(ctx, query, id).Scan(&response.Id, &response.Name, &response.Tin, &response.Latitude, &response.Longitude, &response.GeneralZone, &response.Region, &response.Woreda)
 	if err != nil {
 		switch err {
 		case sql.ErrNoRows:
@@ -70,70 +81,42 @@ func (p *Postgres) GetById(ctx context.Context, id int) (port.GetResponse, error
 			return port.GetResponse{}, port.ErrSysUnknown
 		}
 	}
-
 	return response, nil
 }
 
-func (p *Postgres) UpdateBusiness(ctx context.Context, req *port.UpdateBusinessRequest) (int, error) {
-	var resourceId int
-	query := "SELECT * FROM public.create_distributor_business_location($1, $2, $3, $4, $5, $6);"
-
-	err := p.db.QueryRowContext(ctx, query,
-		req.Id,
-		req.Name,
-		req.Tin,
-		req.DistributorId,
-		req.Location.GeneralZone,
-		req.Location.Woreda,
-		req.Location.Region,
-	).Scan(&resourceId)
-
+func (r *Postgres) UpdateName(ctx context.Context, req *port.UpdateNameRequest) error {
+	query := "SELECT * FROM public.update_distributor_name($1, $2);"
+	_, err := r.Pool.QueryContext(ctx, query, req.Id, req.Name)
 	if err != nil {
 		switch err {
 		case sql.ErrNoRows:
-			return resourceId, port.ErrSysNoRows
+			return port.ErrSysNoRows
 		default:
-			return resourceId, port.ErrSysUnknown
+			return port.ErrSysUnknown
 		}
 	}
-
-	return resourceId, nil
-
+	return nil
 }
 
-func (p *Postgres) CreateBusiness(ctx context.Context, req *port.CreateBusinessInformation) (port.CreateBusinessResponse, error) {
-	var resourceId int
-	var resp port.CreateBusinessResponse
-	query := "SELECT * FROM public.create_distributor_business_location($1, $2, $3, $4, $5, $6);"
-
-	err := p.db.QueryRowContext(ctx, query,
-		req.Name,
-		req.Tin,
-		req.DistributorId,
-		req.GeneralZone,
-		req.Region,
-		req.Woreda,
-	).Scan(&resourceId)
-
+func (r *Postgres) UpdateTin(ctx context.Context, req *port.UpdateTinRequest) error {
+	query := "SELECT * FROM public.update_distributor_tin($1, $2);"
+	_, err := r.Pool.QueryContext(ctx, query, req.Id, req.Tin)
 	if err != nil {
 		switch err {
 		case sql.ErrNoRows:
-			return resp, port.ErrSysNoRows
+			return port.ErrSysNoRows
 		default:
-			return resp, port.ErrSysUnknown
+			return port.ErrSysUnknown
 		}
 	}
-	resp = port.CreateBusinessResponse{
-		BusinessId: resourceId,
-	}
-	return resp, nil
+	return nil
 }
 
-func (p *Postgres) GetBusinessAll(ctx context.Context) (resp port.GetAllResponse, err error) {
+func (r *Postgres) GetAll(ctx context.Context) (port.GetAllResponse, error) {
+	var response port.GetAllResponse
 
 	query := "SELECT * FROM public.get_all_distributors();"
-
-	err = p.db.QueryRowContext(ctx, query).Scan(resp)
+	rows, err := r.Pool.QueryContext(ctx, query)
 	if err != nil {
 		switch err {
 		case sql.ErrNoRows:
@@ -141,48 +124,104 @@ func (p *Postgres) GetBusinessAll(ctx context.Context) (resp port.GetAllResponse
 		default:
 			return port.GetAllResponse{}, port.ErrSysUnknown
 		}
+
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var retailer port.GetResponse
+		if err := rows.Scan(&retailer.Id, &retailer.Name, &retailer.Tin, &retailer.Latitude, &retailer.Longitude, &retailer.GeneralZone, &retailer.Region, &retailer.Woreda); err != nil {
+			log.Printf("unable to scan row: %q", err)
+			return port.GetAllResponse{}, err
+		}
+		response.List = append(response.List, retailer)
 	}
 
-	return resp, nil
-}
-
-func (p *Postgres) GetBusinessById(ctx context.Context, id int) (port.GetBusinessResponse, error) {
-	var response port.GetBusinessResponse
-
-	query := "SELECT * FROM public.get_distributor_by_id($1);"
-
-	err := p.db.QueryRowContext(ctx, query, id).Scan(&response)
-	if err != nil {
-		switch err {
-		case sql.ErrNoRows:
-			return port.GetBusinessResponse{}, port.ErrSysNoRows
-		default:
-			return port.GetBusinessResponse{}, port.ErrSysUnknown
-		}
-	}
-
-	return response, nil
-}
-func (p *Postgres) GetByDistributorId(ctx context.Context, distributor_id int) (port.GetBusinessResponse, error) {
-	var response port.GetBusinessResponse
-
-	query := "SELECT * FROM public.get_distributor_business($1);"
-
-	err := p.db.QueryRowContext(ctx, query, distributor_id).Scan(&response)
-	if err != nil {
-		switch err {
-		case sql.ErrNoRows:
-			return port.GetBusinessResponse{}, port.ErrSysNoRows
-		default:
-			return port.GetBusinessResponse{}, port.ErrSysUnknown
-		}
+	if err := rows.Err(); err != nil {
+		log.Printf("error occurred during rows iteration: %q", err)
+		return port.GetAllResponse{}, port.ErrSysUnknown
 	}
 
 	return response, nil
 }
 
-func NewPostgres(db *sql.DB) port.DB {
-	return &Postgres{
-		db: db,
+func (r *Postgres) GetByName(ctx context.Context, name string) (port.GetAllResponse, error) {
+	var response port.GetAllResponse
+
+	query := "SELECT * FROM public.get_distributor_by_name($1);"
+	rows, err := r.Pool.QueryContext(ctx, query, strings.Trim(name, `"`))
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return port.GetAllResponse{}, port.ErrSysNoRows
+		default:
+			return port.GetAllResponse{}, port.ErrSysUnknown
+		}
+
 	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var retailer port.GetResponse
+		if err := rows.Scan(&retailer.Id, &retailer.Name, &retailer.Tin, &retailer.Latitude, &retailer.Longitude, &retailer.GeneralZone, &retailer.Region, &retailer.Woreda); err != nil {
+			log.Printf("unable to scan row: %q", err)
+			return port.GetAllResponse{}, err
+		}
+		response.List = append(response.List, retailer)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Printf("error occurred during rows iteration: %q", err)
+		return port.GetAllResponse{}, port.ErrSysUnknown
+	}
+
+	return response, nil
+}
+
+func (r *Postgres) GetByTin(ctx context.Context, tin string) (port.GetResponse, error) {
+	var response port.GetResponse
+	query := "SELECT * FROM public.get_distributor_by_tin($1);"
+	err := r.Pool.QueryRowContext(ctx, query, strings.Trim(tin, `"`)).Scan(&response.Id, &response.Name, &response.Tin, &response.Latitude, &response.Longitude, &response.GeneralZone, &response.Region, &response.Woreda)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return port.GetResponse{}, port.ErrSysNoRows
+		default:
+			return port.GetResponse{}, port.ErrSysUnknown
+		}
+	}
+	return response, nil
+}
+
+func (r *Postgres) GetAllUserAgents(ctx context.Context, id int) (port.GetAllUserResponse, error) {
+	var response port.GetAllUserResponse
+
+	query := "SELECT * FROM public.get_all_distributor_users($1);"
+	rows, err := r.Pool.QueryContext(ctx, query, id)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return port.GetAllUserResponse{}, port.ErrSysNoRows
+		default:
+			return port.GetAllUserResponse{}, port.ErrSysUnknown
+		}
+
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var retailer_user port.GetUserResponse
+		if err := rows.Scan(&retailer_user.Id); err != nil {
+			log.Printf("unable to scan row: %q", err)
+			return port.GetAllUserResponse{}, err
+		}
+		response.List = append(response.List, retailer_user)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Printf("error occurred during rows iteration: %q", err)
+		return port.GetAllUserResponse{}, port.ErrSysUnknown
+	}
+
+	return response, nil
 }
