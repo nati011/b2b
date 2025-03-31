5db3974d965d6b66d185e39f938f4ede1811edbf
natnael jemaneh <njemaneh@chegebeya.com>
2025-03-31 17:04:54 +0300

    + implement distributor port

--- internal/port/application/distributor/db.go
+++ internal/port/application/distributor/db.go
@@ -3,7 +3,6 @@ package distributor
 import (
 	"context"
 	"errors"
-	"time"
 )
 
 var (
@@ -12,117 +11,67 @@ var (
 )
 
 type CreateRequest struct {
-	FirstName  string
-	LastName   string
-	Email      string
-	Phone      string
-	Username   string
-	DOB        time.Time
-	ExternalId string
-	Password   string
+	Name        string
+	Tin         string
+	Latitude    string
+	Longitude   string
+	GeneralZone string
+	Region      string
+	Woreda      string
+	UserId      int
 }
-type UpdateRequest struct {
-	Id         int
-	FirstName  string
-	Email      string
-	Phone      string
-	Username   string
-	DOB        time.Time
-	ExternalId string
+
+type UpdateNameRequest struct {
+	Name string
+	Id   int
 }
+
+type UpdateTinRequest struct {
+	Tin string
+	Id  int
+}
+
 type GetResponse struct {
-	Id         int
-	FirstName  string
-	LastName   string
-	Email      string
-	Phone      string
-	Username   string
-	DOB        time.Time
-	ExternalId string
+	Id          int
+	Name        string
+	Tin         string
+	Latitude    string
+	Longitude   string
+	GeneralZone string
+	Region      string
+	Woreda      string
 }
 
 type GetAllResponse struct {
 	List []GetResponse
 }
 
-type GetByIdRequest struct {
+type CreateUserAgentRequest struct {
+	User_id     int
+	Retailer_id int
+}
+
+type GetUserResponse struct {
 	Id int
 }
 
-type RegisterDistributorRequest struct {
-	Email           string    `json:"email"`
-	Password        string    `json:"password"`
-	DOB             time.Time `json:"date_of_birth"`
-	PhoneNumber     string    `json:"phone_number"`
-	ConfirmPassword string    `json:"confirmed_password"`
-	FirstName       string    `json:"first_name"`
-	LastName        string    `json:"last_name"`
-	Username        string    `json:"username"`
-	ExternalId      string    `json:"external_id"`
-}
-
-type BusinessLocation struct {
-	GeneralZone string `json:"general_zone"`
-	Region      string `json:"region"`
-	Woreda      string `json:"woreda"`
-}
-
-type UpdateBusinessRequest struct {
-	Id            int              `json:"id"`
-	DistributorId int              `json:"distributorId"`
-	Name          string           `json:"name"`
-	Tin           int              `json:"tin"`
-	Location      BusinessLocation `json:"location"`
-}
-
-type RegisterDistributorResponse struct {
-	Id      int    `json:"distributorId"`
-	Message string `json:"message"`
-}
-
-type GetByParamRequest struct {
-	Id    int
-	Name  string
-	Email string
-}
-
-type CreateBusinessInformation struct {
-	Name          string `json:"name"`
-	Tin           int    `json:"tin"`
-	Latitude      string `json:"latitude"`
-	Longitude     string `json:"longitude"`
-	GeneralZone   string `json:"general_zone"`
-	Region        string `json:"region"`
-	Woreda        string `json:"woreda"`
-	DistributorId int    `json:"distributorId"`
-}
-
-type CreateBusinessResponse struct {
-	BusinessId int
-}
-type GetBusinessResponse struct {
-	Id            int
-	Name          string
-	Tin           int
-	DistributorId int
-}
-
-type GetAllBusinessResponse struct {
-	List []GetResponse
+type GetAllUserResponse struct {
+	List []GetUserResponse
 }
 
 type Reader interface {
-	GetById(ctx context.Context, id int) (GetResponse, error)
+	Get(ctx context.Context, id int) (GetResponse, error)
 	GetAll(ctx context.Context) (GetAllResponse, error)
-	GetBusinessAll(ctx context.Context) (GetAllResponse, error)
-	GetBusinessById(ctx context.Context, req int) (GetBusinessResponse, error)
-	GetByDistributorId(ctx context.Context, distributorId int) (GetBusinessResponse, error)
+	GetByName(ctx context.Context, name string) (GetAllResponse, error)
+	GetByTin(ctx context.Context, tin string) (GetResponse, error)
+	GetAllUserAgents(ctx context.Context, id int) (GetAllUserResponse, error)
 }
 
 type Writer interface {
-	Create(ctx context.Context, req *CreateRequest) (int, error)
-	CreateBusiness(ctx context.Context, req *CreateBusinessInformation) (CreateBusinessResponse, error)
-	UpdateBusiness(ctx context.Context, req *UpdateBusinessRequest) (int, error)
+	Create(ctx context.Context, req CreateRequest) (int, error)
+	CreateDistributorUser(ctx context.Context, req *CreateUserAgentRequest) (int, error)
+	UpdateName(ctx context.Context, req *UpdateNameRequest) error
+	UpdateTin(ctx context.Context, tin *UpdateTinRequest) error
 }
 
 type DB interface {
