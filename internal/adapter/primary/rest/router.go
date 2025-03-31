fb3ea017d1525d29b569e682beb26d811db1bb8f
natnael jemaneh <njemaneh@chegebeya.com>
2025-03-31 17:05:36 +0300

    + remove distributor import

--- internal/adapter/primary/rest/router.go
+++ internal/adapter/primary/rest/router.go
@@ -13,7 +13,6 @@ import (
 
 func BuildRouter(mux *http.ServeMux, applicationServices *application_core.Container, domainServices *domain_core.Container) error {
 	application_handler.InitAuth()
-	application_handler.InitDistributor()
 	application_handler.InitHealth()
 	application_handler.InitResource()
 
