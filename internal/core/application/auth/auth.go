56be6f8f881bb6035afaf7555a012b2fdcd184f6
natnael jemaneh <njemaneh@chegebeya.com>
2025-04-01 17:05:35 +0300

    + cleanup on auth-keycloak integration

--- internal/core/application/auth/auth.go
+++ internal/core/application/auth/auth.go
@@ -3,7 +3,7 @@ package auth
 import (
 	"context"
 	"errors"
-	"log"
+	"time"
 
 	port "b2b.nati011.github.com/internal/port/application/auth/provider"
 )
@@ -20,85 +20,120 @@ var (
 	ErrFirstNameNotSupplied            = errors.New("oopsy, First Name mandatory")
 	ErrLastNameNotSupplied             = errors.New("oppsy, Last Name mandatory")
 	ErrPasswordsDontMatch              = errors.New("oopsy, passwords dont match")
-
-	SUCCESS_MESSAGE = "Ahoy!"
 )
 
+type RegisterUserRequest struct {
+	Email           string    `json:"email"`
+	Password        string    `json:"password"`
+	ConfirmPassword string    `json:"confirmed_password"`
+	BirthDate       time.Time `json:"birth_date"`
+	PhoneNumber     string    `json:"phone_number"`
+	ExternalId      string    `json:"external_id"`
+	FirstName       string    `json:"first_name"`
+	LastName        string    `json:"last_name"`
+	Username        string    `json:"username"`
+}
+
+type RegisterUserResponse struct {
+	Username string `json:"username"`
+}
+
+type RefreshTokenRequest struct {
+	RefreshToken string `json:"refreshToken"`
+}
+
+type LoginAuthResponse struct {
+	JWT JWT
+}
+
+type LoginUserRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type JWT struct {
+	AccessToken      string `json:"access_token"`
+	IDToken          string `json:"id_token"`
+	ExpiresIn        int    `json:"expires_in"`
+	RefreshExpiresIn int    `json:"refresh_expires_in"`
+	RefreshToken     string `json:"refresh_token"`
+	TokenType        string `json:"token_type"`
+	NotBeforePolicy  int    `json:"not_before_policy"`
+	SessionState     string `json:"session_state"`
+	Scope            string `json:"scope"`
+}
+
 type Provider interface {
-	CreateNewClient(ctx context.Context, req port.RegisterUserRequest) (port.RegisterUserResponse, error)
-	ClientLogin(ctx context.Context, req port.LoginUserRequest) (port.LoginAuthResponse, error)
-	RefreshToken(ctx context.Context, req port.RefreshTokenRequest) (port.LoginAuthResponse, error)
+	CreateNewClient(ctx context.Context, req RegisterUserRequest) (RegisterUserResponse, error)
+	ClientLogin(ctx context.Context, req LoginUserRequest) (LoginAuthResponse, error)
+	RefreshToken(ctx context.Context, req RefreshTokenRequest) (LoginAuthResponse, error)
 }
 
 type AuthService struct {
-	authProvider Provider
+	authProvider port.Provider
 }
 
-func NewAuthService(ap port.Provider) port.Provider {
+func NewAuthService(ap port.Provider) Provider {
 	return &AuthService{authProvider: ap}
 
 }
 
-func (a *AuthService) CreateNewClient(ctx context.Context, req port.RegisterUserRequest) (port.RegisterUserResponse, error) {
+func (a *AuthService) CreateNewClient(ctx context.Context, req RegisterUserRequest) (RegisterUserResponse, error) {
 	err := validateName(req.FirstName, req.LastName)
 	if err != nil {
-		return port.RegisterUserResponse{}, err
+		return RegisterUserResponse{}, err
 	}
 
 	err = validatePasswords(req.Password, req.ConfirmPassword)
 	if err != nil {
-		return port.RegisterUserResponse{}, err
+		return RegisterUserResponse{}, err
 	}
 	err = validateEmail(req.Email)
 	if err != nil {
-		return port.RegisterUserResponse{}, err
+		return RegisterUserResponse{}, err
 	}
 
-	resp, err := a.authProvider.CreateNewClient(ctx, req)
+	resp, err := a.authProvider.CreateNewClient(ctx, port.RegisterUserRequest(req))
 	if err != nil {
 		switch err {
 		case port.ErrSysUsernameTaken:
-			return port.RegisterUserResponse{}, ErrUsernameTaken
+			return RegisterUserResponse{}, ErrUsernameTaken
 		case port.ErrSysEmailTaken:
-			return port.RegisterUserResponse{}, ErrEmailTaken
+			return RegisterUserResponse{}, ErrEmailTaken
 		default:
-			return port.RegisterUserResponse{}, ErrUnknown
+			return RegisterUserResponse{}, ErrUnknown
 		}
 	}
-	return port.RegisterUserResponse{
+	return RegisterUserResponse{
 		Username: resp.Username,
 	}, nil
 }
 
-func (a *AuthService) ClientLogin(ctx context.Context, rq port.LoginUserRequest) (port.LoginAuthResponse, error) {
-	resp, err := a.authProvider.ClientLogin(ctx, rq)
-	log.Printf(resp.Message)
+func (a *AuthService) ClientLogin(ctx context.Context, rq LoginUserRequest) (LoginAuthResponse, error) {
+	resp, err := a.authProvider.ClientLogin(ctx, port.LoginUserRequest(rq))
 	if err != nil {
 		switch err {
 		case port.ErrSysFailedToLogin:
-			return port.LoginAuthResponse{}, ErrFailedToLogin
+			return LoginAuthResponse{}, ErrFailedToLogin
 		default:
-			return port.LoginAuthResponse{}, ErrUnknown
+			return LoginAuthResponse{}, ErrUnknown
 		}
 	}
-	return port.LoginAuthResponse{
-		JWT: port.JWT(resp.JWT),
+	return LoginAuthResponse{
+		JWT: JWT(resp.JWT),
 	}, nil
 }
-func (a *AuthService) RefreshToken(ctx context.Context, req port.RefreshTokenRequest) (port.LoginAuthResponse, error) {
-	resp, err := a.authProvider.RefreshToken(ctx, req)
+func (a *AuthService) RefreshToken(ctx context.Context, req RefreshTokenRequest) (LoginAuthResponse, error) {
+	resp, err := a.authProvider.RefreshToken(ctx, port.RefreshTokenRequest(req))
 	if err != nil {
 		switch err {
 		case port.ErrSysFailedToLogin:
-			return port.LoginAuthResponse{
-				Message: ErrFailedToLogin.Error(),
-			}, ErrFailedToLogin
+			return LoginAuthResponse{}, ErrFailedToLogin
 		default:
-			return port.LoginAuthResponse{}, ErrUnknown
+			return LoginAuthResponse{}, ErrUnknown
 		}
 	}
-	return port.LoginAuthResponse{
-		JWT:     port.JWT(resp.JWT),
-		Message: SUCCESS_MESSAGE,
+	return LoginAuthResponse{
+		JWT: JWT(resp.JWT),
 	}, nil
 }
