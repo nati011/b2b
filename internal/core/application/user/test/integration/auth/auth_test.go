b60c23092bee7a44ab8fa08938a664b4a6c1df86
natnael jemaneh <njemaneh@chegebeya.com>
2025-04-03 10:39:53 +0300

    + extract test boilerplate auth test container setup to utils

--- internal/core/application/user/test/integration/auth/auth_test.go
+++ internal/core/application/user/test/integration/auth/auth_test.go
@@ -3,22 +3,17 @@ package auth
 import (
 	"context"
 	"database/sql"
-	"fmt"
-	"log"
 	"os"
-	"strings"
 	"testing"
 	"time"
 
 	"b2b.nati011.github.com/internal/core/application/auth"
 	"b2b.nati011.github.com/internal/core/application/user"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/modules/postgres"
-	"github.com/testcontainers/testcontainers-go/wait"
+	db_test_container "b2b.nati011.github.com/internal/core/util/test_container/db"
 )
 
 var testContainer user.TestContainer
-var pgContainer *postgres.PostgresContainer
+
 var db *sql.DB
 
 func TestMain(m *testing.M) {
@@ -28,121 +23,18 @@ func TestMain(m *testing.M) {
 }
 
 func setup() {
-	var err error
-	ctx := context.Background()
-	pgContainer, err = RunContainer(ctx)
-	if err != nil {
-		panic(err)
-	}
-
-	connectionString, err := pgContainer.ConnectionString(ctx)
-	if err != nil {
-		panic(err)
-	}
-
-	db, err = sql.Open("pgx", connectionString)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := db.PingContext(ctx); err != nil {
-		log.Fatal(err)
-	}
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	// ddl
-	err = runMigration(db, "/home/natanel/personal/b2b_clean/b2b/migration/core_db.sql")
-	if err != nil {
-		log.Fatalf("Error running ddl migration: %v", err)
-	}
-
-	// functions
-	err = runMigration(db, "/home/natanel/personal/b2b_clean/b2b/migration/core_db_functions.sql")
-	if err != nil {
-		log.Fatalf("Error running stored func migration: %v", err)
-	}
+	db = db_test_container.Setup()
 	testContainer = user.NewIntegrationTestContainer(
 		db,
 	)
 }
 
 func teardown() {
-	// Start a transaction
-	tx, err := db.Begin()
-	if err != nil {
-		log.Fatalf("could not begin transaction: %v", err)
-	}
-
-	// Get all table names
-	var tables []string
-	rows, err := tx.Query("SELECT tablename FROM pg_tables WHERE schemaname = 'public';")
-	if err != nil {
-		tx.Rollback()
-		log.Fatalf("could not fetch table names: %v", err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var table string
-		if err := rows.Scan(&table); err != nil {
-			tx.Rollback()
-			log.Fatalf("could not scan table name: %v", err)
-		}
-		tables = append(tables, table)
-	}
-
-	// Prepare the TRUNCATE statement
-	if len(tables) > 0 {
-		truncateQuery := "TRUNCATE TABLE " + strings.Join(tables, ", ") + " RESTART IDENTITY CASCADE;"
-		_, err = tx.Exec(truncateQuery)
-		if err != nil {
-			tx.Rollback()
-			log.Fatalf("could not truncate tables: %v", err)
-		}
-	}
-
-	// Commit the transaction
-	if err := tx.Commit(); err != nil {
-		log.Fatalf("could not commit transaction: %v", err)
-	}
-}
-
-func runMigration(db *sql.DB, filename string) error {
-	// Read the SQL file
-	sqlBytes, err := os.ReadFile(filename)
-	if err != nil {
-		return fmt.Errorf("could not read file: %w", err)
-	}
-
-	// Execute the SQL
-	_, err = db.Exec(string(sqlBytes))
-	if err != nil {
-		return fmt.Errorf("could not execute SQL: %w", err)
-	}
-
-	return nil
-}
-
-func RunContainer(ctx context.Context) (*postgres.PostgresContainer, error) {
-	return postgres.Run(ctx,
-		"postgres:16-alpine",
-		postgres.WithDatabase("test"),
-		postgres.WithUsername("user"),
-		postgres.WithPassword("password"),
-
-		testcontainers.WithWaitStrategy(
-			wait.ForLog("database system is ready to accept connections").
-				WithOccurrence(2).
-				WithStartupTimeout(30*time.Second),
-		),
-	)
+	db_test_container.Teardown(db)
 }
 
 func Test_create_auth_client_upon_user_registration(t *testing.T) {
-	t.Cleanup(teardown)
+	// t.Cleanup(teardown)
 	ctx := context.Background()
 	//setup
 	parsedTime, _ := time.Parse("2006-01-02 15:04:05", "2024-09-19 14:00:00")
