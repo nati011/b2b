d66cbd5346c2ac862a66f97cac785f50b50ce08c
natnael jemaneh <njemaneh@chegebeya.com>
2025-04-07 15:52:34 +0300

    + implement pg adapter

--- internal/adapter/secondary/domain/order/db/pg.go
+++ internal/adapter/secondary/domain/order/db/pg.go
@@ -3,6 +3,7 @@ package order
 import (
 	"context"
 	"database/sql"
+	"log"
 
 	port "b2b.nati011.github.com/internal/port/domain/order"
 )
@@ -17,26 +18,184 @@ func NewPostgres(DB *sql.DB) port.DB {
 	}
 }
 
-func (p *Postgres) GetByID(context.Context, int) (port.GetResponse, error) {
-	return port.GetResponse{}, nil
+func (p *Postgres) GetByID(ctx context.Context, id int) (port.GetResponse, error) {
+	var response port.GetResponse
+	query := "SELECT * FROM public.get_orders_by_id($1);"
+	err := p.Pool.QueryRowContext(ctx, query, id).Scan(
+		&response.Id,
+		&response.RetailerId,
+		&response.Status,
+		&response.Total)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return port.GetResponse{}, port.ErrSysNoRows
+		default:
+			return port.GetResponse{}, port.ErrSysUnknown
+		}
+	}
+	return response, nil
 }
 
-func (p *Postgres) GetByRetailerID(context.Context, int) (port.GetAllResponse, error) {
-	return port.GetAllResponse{}, nil
+func (p *Postgres) GetByRetailerID(ctx context.Context, retailerId int) (port.GetAllResponse, error) {
+	var response port.GetAllResponse
+
+	query := "SELECT * FROM public.get_orders_by_retailer_id($1);"
+	rows, err := p.Pool.QueryContext(ctx, query, retailerId)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return port.GetAllResponse{}, port.ErrSysNoRows
+		default:
+			return port.GetAllResponse{}, port.ErrSysUnknown
+		}
+
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var order port.GetResponse
+		if err := rows.Scan(
+			&order.Id,
+			&order.RetailerId,
+			&order.Status,
+			&order.Total); err != nil {
+
+			log.Printf("unable to scan row: %q", err)
+			return port.GetAllResponse{}, err
+		}
+		response.List = append(response.List, order)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Printf("error occurred during rows iteration: %q", err)
+		return port.GetAllResponse{}, port.ErrSysUnknown
+	}
+
+	return response, nil
 }
 
-func (p *Postgres) GetByStatus(context.Context, string) (port.GetAllResponse, error) {
-	return port.GetAllResponse{}, nil
+func (p *Postgres) GetByStatus(ctx context.Context, status string) (port.GetAllResponse, error) {
+	var response port.GetAllResponse
+
+	query := "SELECT * FROM public.get_orders_by_status($1);"
+	rows, err := p.Pool.QueryContext(ctx, query, status)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return port.GetAllResponse{}, port.ErrSysNoRows
+		default:
+			return port.GetAllResponse{}, port.ErrSysUnknown
+		}
+
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var order port.GetResponse
+		if err := rows.Scan(
+			&order.Id,
+			&order.RetailerId,
+			&order.Status,
+			&order.Total); err != nil {
+
+			log.Printf("unable to scan row: %q", err)
+			return port.GetAllResponse{}, err
+		}
+		response.List = append(response.List, order)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Printf("error occurred during rows iteration: %q", err)
+		return port.GetAllResponse{}, port.ErrSysUnknown
+	}
+
+	return response, nil
 }
 
-func (p *Postgres) GetAll(context.Context) (port.GetAllResponse, error) {
-	return port.GetAllResponse{}, nil
+func (p *Postgres) GetAll(ctx context.Context) (port.GetAllResponse, error) {
+	var response port.GetAllResponse
+
+	query := "SELECT * FROM public.get_all_orders();"
+	rows, err := p.Pool.QueryContext(ctx, query)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return port.GetAllResponse{}, port.ErrSysNoRows
+		default:
+			return port.GetAllResponse{}, port.ErrSysUnknown
+		}
+
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var order port.GetResponse
+		if err := rows.Scan(
+			&order.Id,
+			&order.RetailerId,
+			&order.Status,
+			&order.Total); err != nil {
+
+			log.Printf("unable to scan row: %q", err)
+			return port.GetAllResponse{}, err
+		}
+		response.List = append(response.List, order)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Printf("error occurred during rows iteration: %q", err)
+		return port.GetAllResponse{}, port.ErrSysUnknown
+	}
+
+	return response, nil
 }
 
-func (p *Postgres) Create(context.Context, *port.CreateRequest) (int, error) {
-	return 0, nil
+func (p *Postgres) Create(ctx context.Context, req *port.CreateRequest) (int, error) {
+	var orderId int
+	query := "SELECT * FROM public.create_order($1, $2, $3);"
+
+	err := p.Pool.QueryRowContext(ctx, query,
+		req.RetailerId,
+		req.Status,
+	).Scan(&orderId)
+	if err != nil {
+		switch err {
+		default:
+			return 0, port.ErrSysUnknown
+		}
+	}
+
+	//orderItem
+	for _, i := range req.Items {
+		query = "SELECT * FROM public.create_order_item($1, $2, $3, $4);"
+		err := p.Pool.QueryRowContext(ctx, query,
+			orderId,
+			i.ProductId,
+			i.Quantity,
+			i.Price,
+		).Scan()
+		if err != nil {
+			switch err {
+			default:
+				return 0, port.ErrSysUnknown
+			}
+		}
+	}
+
+	return orderId, nil
 }
 
-func (p *Postgres) UpdateOrderStatus(context.Context, *port.UpdateOrderStatusRequest) error {
+func (p *Postgres) UpdateOrderStatus(ctx context.Context, req *port.UpdateOrderStatusRequest) error {
+	query := "SELECT * FROM public.update_order_status($1, $2);"
+	_, err := p.Pool.QueryContext(ctx, query, req.Id, req.Status)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return port.ErrSysNoRows
+		default:
+			return port.ErrSysUnknown
+		}
+	}
 	return nil
 }
