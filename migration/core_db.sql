02135d98f9a60737b7e8f13c23ed5cb435de0293
natnael jemaneh <njemaneh@chegebeya.com>
2025-04-07 18:56:38 +0300

    + resolve issue with ddl and procedures

--- migration/core_db.sql
+++ migration/core_db.sql
@@ -172,7 +172,7 @@ CREATE TABLE IF NOT EXISTS public."products"
 (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
-  description TEXT,
+  description VARCHAR(255),
   external_id VARCHAR(255),
   is_active BOOLEAN DEFAULT FALSE,
   distributor_id INT
@@ -214,7 +214,7 @@ COMMENT ON TABLE public."s_ledger" IS 'ledger for stock movement';
 
 CREATE TABLE IF NOT EXISTS public."p_prices" 
 (
-  price DECIMAL(12, 2),
+  price DECIMAL(12,2),
   product_id INT,
   FOREIGN KEY (product_id) REFERENCES public."products" (id) ON DELETE CASCADE
 ) INHERITS (public."base");
@@ -290,7 +290,7 @@ CREATE TABLE IF NOT EXISTS public."cp_images"
   url VARCHAR(255),
   blur_hash VARCHAR(255),
   configurable_product_id INT,
-    FOREIGN KEY (configurable_product_id) REFERENCES public."configurable_products"(id) ON DELETE CASCADE
+  FOREIGN KEY (configurable_product_id) REFERENCES public."configurable_products"(id) ON DELETE CASCADE
 ) INHERITS(public."base");
 
 COMMENT ON TABLE public."p_attributes" IS 'stores images of products';
@@ -300,9 +300,8 @@ CREATE TABLE IF NOT EXISTS public."orders"
   id SERIAL PRIMARY KEY,
   retailer_id INT,
   status VARCHAR(255),
-  total MONEY,
-	FOREIGN KEY (retailer_id) REFERENCES public."users"(id) ON DELETE CASCADE,
-	FOREIGN KEY (status_id) REFERENCES public."o_statuses"(id) ON DELETE CASCADE
+  total DECIMAL(12,2),
+	FOREIGN KEY (retailer_id) REFERENCES public."users"(id) ON DELETE CASCADE
 ) INHERITS(public."base");
 
 COMMENT ON TABLE public."orders" IS 'stores orders';
@@ -312,7 +311,7 @@ CREATE TABLE IF NOT EXISTS public."o_items"
   order_id INT,
   product_id INT,
   quantity INT,
-  price DECIMAL(12, 2),
+  price DECIMAL(12,2),
   FOREIGN KEY (order_id) REFERENCES public."orders" (id) ON DELETE CASCADE,
   FOREIGN KEY (product_id) REFERENCES public."products" (id) ON DELETE CASCADE
 ) INHERITS (public."base");
