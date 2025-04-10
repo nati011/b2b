40995dc44fdd22706d8196ecf164086eb2f38550
expired-pineapple <ruthtirusew944@gmail.com>
2025-04-10 17:53:44 +0300

    + mock for delete client

--- migration/core_db_functions.sql
+++ migration/core_db_functions.sql
@@ -603,17 +603,17 @@ BEGIN
 END;
 $$;
 
-create or replace function public.get_user_provider(
-id INT) Returns TABLE(
-user_id INT,
-provider_id VARCHAR(255))
+CREATE OR REPLACE function public.get_user_provider(id INT) 
+RETURNS TABLE(user_id INT,
+              provider_id VARCHAR(255))
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    SELECT 
-    up.user_id, 
-    up.provider_id FROM public.user_providers up 
-    WHERE up.is_deleted = FALSE AND up.user_id=id;
+    RETURN QUERY
+    SELECT up.user_id, up.provider_id 
+    FROM public.user_providers up 
+    WHERE up.is_deleted = FALSE 
+    AND up.user_id=id;
 
 END;
 $$;
