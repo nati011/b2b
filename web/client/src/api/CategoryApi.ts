dcfa2d2d0bb90474f11e5a3df946a5025620c15d
Alazar <aandualem@chegebeya.com>
2025-04-16 16:56:59 +0300

    Product and category api implementation

--- web/client/src/api/CategoryApi.ts
+++ web/client/src/api/CategoryApi.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 interface Category {
-  Name: string;
-  // Add other properties if needed
+  id: number;
+  name: string;
 }
 
 const API_URL = "/api/v1/category";
@@ -10,9 +10,9 @@ const API_URL = "/api/v1/category";
 export const fetchCategories = async (): Promise<Category[]> => {
   try {
     const response = await axios.get(API_URL);
-    return response.data.categories?.List || [];
+    return response.data.body.category.categories || [];
   } catch (error) {
     console.error("Error fetching categories:", error);
-    throw error; // Re-throw the error for handling in the component
+    throw error;
   }
 };
