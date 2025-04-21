6f6f6d1ae87fa8249ec3de08ba46650683a48803
natnael jemaneh <njemaneh@chegebeya.com>
2025-04-21 19:18:42 +0300

    + invoke order status change from paymentProcessor

--- internal/core/domain/paymentProcessor/processor.go
+++ internal/core/domain/paymentProcessor/processor.go
@@ -1,6 +1,10 @@
 package payment_processor
 
 import (
+	"context"
+	"log"
+	"strconv"
+
 	"b2b.nati011.github.com/internal/core/domain/order"
 )
 
@@ -8,6 +12,18 @@ type Provider struct {
 	orderService order.Provider
 }
 
-func (p *Provider) Process(tx_ref string) {
-	//change order payment status to PAYMENT_ACCEPTED
+func (p *Provider) Process(ctx context.Context, tx_ref string) {
+	order_id, err := strconv.Atoi(tx_ref)
+	if err != nil {
+		log.Printf("Failed to process payment for tx_ref: %v", tx_ref)
+		return
+	}
+	_, err = p.orderService.UpdatePaymentStatus(ctx, &order.UpdateRequest{
+		Id:            order_id,
+		PaymentStatus: order.PAYMENT_ACCEPTED_STATUS,
+	})
+	if err != nil {
+		log.Printf("Failed to update order payment status for tx_ref: %v", tx_ref)
+		return
+	}
 }
