fb18c02ef627df20bea2886fc66ea5be1c174000
natnael jemaneh <njemaneh@chegebeya.com>
2025-04-21 18:32:39 +0300

    + add update order delivery and payment statuses with ops:

--- internal/port/domain/order/db.go
+++ internal/port/domain/order/db.go
@@ -16,18 +16,22 @@ type Item struct {
 	Price     float64
 }
 type CreateRequest struct {
-	RetailerId int
-	Items      []Item
-	Status     string
-	Total      float64
+	RetailerId     int
+	Items          []Item
+	Status         string
+	PaymentStatus  string
+	DeliveryStatus string
+	Total          float64
 }
 
 type GetResponse struct {
-	Id         int
-	RetailerId int
-	Items      []Item
-	Total      float64
-	Status     string
+	Id             int
+	RetailerId     int
+	Items          []Item
+	Total          float64
+	Status         string
+	PaymentStatus  string
+	DeliveryStatus string
 }
 
 type GetAllResponse struct {
@@ -37,6 +41,16 @@ type GetAllResponse struct {
 type UpdateOrderStatusRequest struct {
 	Id     int
 	Status string
+}
+
+type UpdateOrderDeliveryStatusRequest struct {
+	Id             int
+	DeliveryStatus string
+}
+
+type UpdateOrderPaymentStatusRequest struct {
+	Id            int
+	PaymentStatus string
 }
 
 type Reader interface {
@@ -49,6 +63,8 @@ type Reader interface {
 type Writer interface {
 	Create(context.Context, *CreateRequest) (int, error)
 	UpdateOrderStatus(context.Context, *UpdateOrderStatusRequest) error
+	UpdatePaymentStatus(context.Context, *UpdateOrderPaymentStatusRequest) error
+	UpdateDeliveryStatus(context.Context, *UpdateOrderDeliveryStatusRequest) error
 }
 
 type DB interface {
