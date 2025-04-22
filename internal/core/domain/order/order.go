776530c3ce50711ad6eedfeb6ab92af67331c845
natnael jemaneh <njemaneh@chegebeya.com>
2025-04-22 11:11:35 +0300

    + resolve conflict

--- internal/core/domain/order/order.go
+++ internal/core/domain/order/order.go
@@ -401,7 +401,6 @@ func (o *OrderService) UpdateStatus(ctx context.Context, req *UpdateRequest) (in
 		switch err {
 		default:
 			return 0, ErrUnknown
-<<<<<<< HEAD
 		}
 	}
 
@@ -431,48 +430,14 @@ func (o *OrderService) UpdatePaymentStatus(ctx context.Context, req *UpdateReque
 		switch err {
 		default:
 			return 0, ErrUnknown
-=======
->>>>>>> b8531fb5 (+ add update order delivery and payment statuses with ops:)
 		}
 	}
 
-	// deplete stock if order status is COMPELETED
+	//TODO: reserve stock
 
 	return resp.Id, nil
 }
 
-<<<<<<< HEAD
-=======
-func (o *OrderService) UpdatePaymentStatus(ctx context.Context, req *UpdateRequest) (int, error) {
-	//validate
-	resp, err := o.Get(ctx, req.Id)
-	if err != nil {
-		switch err {
-		case port.ErrSysNoRows:
-			return 0, ErrIdNotFound
-		default:
-			return 0, ErrUnknown
-		}
-	}
-
-	//update
-	err = o.DB.UpdatePaymentStatus(ctx, &port.UpdateOrderPaymentStatusRequest{
-		Id:            req.Id,
-		PaymentStatus: req.Status,
-	})
-	if err != nil {
-		switch err {
-		default:
-			return 0, ErrUnknown
-		}
-	}
-
-	// deplete stock if order status is COMPELETED
-
-	return resp.Id, nil
-}
-
->>>>>>> b8531fb5 (+ add update order delivery and payment statuses with ops:)
 func (o *OrderService) UpdateDeliveryStatus(ctx context.Context, req *UpdateRequest) (int, error) {
 	//validate
 	resp, err := o.Get(ctx, req.Id)
@@ -487,13 +452,8 @@ func (o *OrderService) UpdateDeliveryStatus(ctx context.Context, req *UpdateRequ
 
 	//update
 	err = o.DB.UpdateDeliveryStatus(ctx, &port.UpdateOrderDeliveryStatusRequest{
-<<<<<<< HEAD
 		Id:             req.Id,
 		DeliveryStatus: req.DeliveryStatus,
-=======
-		Id:     req.Id,
-		Status: req.Status,
->>>>>>> b8531fb5 (+ add update order delivery and payment statuses with ops:)
 	})
 	if err != nil {
 		switch err {
