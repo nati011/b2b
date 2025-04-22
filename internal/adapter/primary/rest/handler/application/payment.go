0c67118602dfee43a871651bd5534590ba9be62c
natnael jemaneh <njemaneh@chegebeya.com>
2025-04-22 11:09:16 +0300

    + add removed order changes

---
+++ internal/adapter/primary/rest/handler/application/payment.go
@@ -0,0 +1,47 @@
+package application
+
+import (
+	"errors"
+	"net/http"
+
+	"b2b.nati011.github.com/internal/adapter/primary/rest/handler"
+	util "b2b.nati011.github.com/internal/adapter/primary/rest/handler/util"
+	application_core "b2b.nati011.github.com/internal/core/application"
+	"b2b.nati011.github.com/internal/core/application/payment"
+	domain_core "b2b.nati011.github.com/internal/core/domain"
+)
+
+var (
+	ErrUnknownPaymentCommand = errors.New("unknown user command")
+)
+
+type Payment struct {
+	service payment.Provider
+}
+
+func InitPayment() {
+	handler.Register(new(Payment))
+}
+
+func (p *Payment) Init(applicationServices *application_core.Container, domainService *domain_core.Container) error {
+	p.service = applicationServices.PaymentService
+	return nil
+}
+
+func (p *Payment) Routes(mux *http.ServeMux) {
+	mux.HandleFunc("GET /api/v1/payment/webhook/{gateway_id}/{tx_ref}", p.CallbackHandler)
+}
+
+func (p *Payment) CallbackHandler(w http.ResponseWriter, r *http.Request) {
+	typedParamGatewayId, err := util.GetPathParam(r, 5)
+	if err != nil {
+		util.RequestErrorResponse(w, err)
+		return
+	}
+	typedParamTxRef, err := util.GetStringPathParam(r, 6)
+	if err != nil {
+		util.RequestErrorResponse(w, err)
+		return
+	}
+	p.service.Callback(r.Context(), typedParamGatewayId, typedParamTxRef)
+}
