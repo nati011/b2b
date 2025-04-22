67b66d3dce5351917237b45533faa13109b1d5b6
natnael jemaneh <njemaneh@chegebeya.com>
2025-04-22 09:59:11 +0300

    + payment callback handler

--- internal/adapter/primary/rest/handler/application/payment.go
+++ internal/adapter/primary/rest/handler/application/payment.go
@@ -44,6 +44,4 @@ func (p *Payment) CallbackHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	p.service.Callback(r.Context(), typedParamGatewayId, typedParamTxRef)
-	tx_ref := "0"
-	p.service.Callback(r.Context(), typedParamGatewayId, tx_ref)
 }
