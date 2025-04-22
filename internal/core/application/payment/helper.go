9899c7bd0d8d51a465b3efab707ff911923fb6c7
natnael jemaneh <njemaneh@chegebeya.com>
2025-04-22 12:04:31 +0300

    + resolve conflict

--- internal/core/application/payment/helper.go
+++ internal/core/application/payment/helper.go
@@ -1,18 +1,1 @@
 package payment
-
-func (p *PaymentService) validateAmount(amount float64) error {
-	if amount == 0 {
-		return ErrAmountNotSupplied
-	}
-	if amount < 0 {
-		return ErrAmountLessThanZero
-	}
-	return nil
-}
-
-func verifyTransactionRef(tx_ref string) error {
-	if tx_ref == "" {
-		return ErrTransactionReferenceNotSupplied
-	}
-	return nil
-}
