f40a59a5f565eaa6bbf4c7cd2a3b21219a2879c1
expired-pineapple <ruthtirusew944@gmail.com>
2025-04-22 16:28:47 +0300

    init payment verifications

--- internal/adapter/secondary/application/payment/gateway/chapa.go
+++ internal/adapter/secondary/application/payment/gateway/chapa.go
@@ -24,6 +24,18 @@ type InitatePaymentChapaResponse struct {
 	Data    InitatePaymentChapaResponseData `json:"data"`
 }
 
+type VerifyPaymentChapaResponseData struct {
+	Amount         float64 `json:"amount"`
+	Status         string  `json:"status"`
+	TransactionRef string  `json:"tx_ref"`
+}
+
+type VerifyPaymentChapaResponse struct {
+	Message string                          `json:"message"`
+	Status  string                          `json:"status"`
+	Data    InitatePaymentChapaResponseData `json:"data"`
+}
+
 type Chapa struct {
 }
 
@@ -31,13 +43,13 @@ func NewChapa() port.Provider {
 	return &Chapa{}
 }
 
-func (t Chapa) Initiate(request port.InitiateRequest) (string, error) {
+func (t Chapa) Initiate(request port.InitiateRequest) (port.InitatePaymentResponse, error) {
 	url := request.PartnerUrl
 	var response InitatePaymentChapaResponse
 
 	payload, err := json.Marshal(request)
 	if err != nil {
-		return "", port.ErrUnknown
+		return port.InitatePaymentResponse{}, port.ErrUnknown
 	}
 
 	client := &http.Client{}
@@ -45,34 +57,74 @@ func (t Chapa) Initiate(request port.InitiateRequest) (string, error) {
 	req, err := http.NewRequest("POST", url, body)
 
 	if err != nil {
-		return "", port.ErrUnknown
+		return port.InitatePaymentResponse{}, port.ErrUnknown
 	}
 
 	req.Header.Add("Authorization", request.PartnerSecret)
 	res, err := client.Do(req)
 
 	if err != nil {
-		return "", port.ErrUnknown
+		return port.InitatePaymentResponse{}, port.ErrUnknown
 	}
 
 	defer res.Body.Close()
 
 	resBody, err := io.ReadAll(res.Body)
 	if err != nil {
-		return "", port.ErrUnknown
+		return port.InitatePaymentResponse{}, port.ErrUnknown
 	}
 
 	if err := json.Unmarshal(resBody, &response); err != nil {
-		return "", port.ErrUnknown
+		return port.InitatePaymentResponse{}, port.ErrUnknown
 	}
 
 	switch response.Status {
 	case "success":
-		return response.Data.CheckoutUrl, nil
+		return port.InitatePaymentResponse{
+			CheckoutUrl: response.Data.CheckoutUrl}, nil
 	default:
-		return "", port.ErrUnknown
+		return port.InitatePaymentResponse{}, port.ErrUnknown
 	}
 
 }
 
-func (t Chapa) Verify() {}
+func (t Chapa) Verify(request port.VerifyRequest) (port.VerifyResponse, error) {
+	url := request.PartnerUrl
+	var response VerifyPaymentChapaResponse
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		return port.VerifyResponse{}, port.ErrUnknown
+	}
+
+	req.Header.Add("Authorization", request.PartnerSecret)
+	res, err := client.Do(req)
+
+	if err != nil {
+		return port.VerifyResponse{}, port.ErrUnknown
+	}
+
+	defer res.Body.Close()
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return port.VerifyResponse{}, port.ErrUnknown
+	}
+
+	if err := json.Unmarshal(resBody, &response); err != nil {
+		return port.VerifyResponse{}, port.ErrUnknown
+	}
+
+	switch response.Status {
+	case "success":
+		return port.VerifyResponse{
+			Status: response.Status,
+		}, nil
+	case "failed":
+		return port.VerifyResponse{}, port.ErrInvalidTransaction
+	default:
+		return port.VerifyResponse{}, port.ErrUnknown
+	}
+}
--- internal/port/application/payment/gateway/gateway.go
+++ internal/port/application/payment/gateway/gateway.go
@@ -3,18 +3,34 @@ package payment
 import "errors"
 
 var (
-	ErrUnknown = errors.New("oopsy, Unknown error has occured")
+	ErrUnknown            = errors.New("oopsy, Unknown error has occured")
+	ErrInvalidTransaction = errors.New("oopsy, the transaction is invalid")
 )
 
 type InitiateRequest struct {
 	Amount         float64 `json:"amount"`
-	TransactionRef int     `json:"tx_ref"`
-	ReturnUrl      string  `json:"return_url"`
+	CallbackUrl    string  `json:"callback_url"`
 	PartnerUrl     string  `json:"partner_url"`
 	PartnerSecret  string  `json:"partner_secret"`
+	ReturnUrl      string  `json:"return_url"`
+	TransactionRef int     `json:"tx_ref"`
+}
+
+type InitatePaymentResponse struct {
+	CheckoutUrl string
+}
+
+type VerifyRequest struct {
+	PartnerUrl     string
+	TransactionRef string
+	PartnerSecret  string
+}
+
+type VerifyResponse struct {
+	Status string
 }
 
 type Provider interface {
-	Initiate(req InitiateRequest) (string, error)
-	Verify()
+	Initiate(req InitiateRequest) (InitatePaymentResponse, error)
+	Verify(VerifyRequest) (VerifyResponse, error)
 }
