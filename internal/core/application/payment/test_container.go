edd1470add93ee0ea07b60bbc49f6b0032bcdbb4
natnael jemaneh <njemaneh@chegebeya.com>
2025-04-22 12:04:31 +0300

    + resolve conflict

--- internal/core/application/payment/test_container.go
+++ internal/core/application/payment/test_container.go
@@ -4,6 +4,7 @@ import (
 	partner "b2b.nati011.github.com/internal/core/application/payment_partner"
 	"b2b.nati011.github.com/internal/core/application/transaction"
 	"b2b.nati011.github.com/internal/core/application/user"
+	payment_processor "b2b.nati011.github.com/internal/core/domain/paymentProcessor"
 )
 
 type TestContainer struct {
@@ -11,24 +12,17 @@ type TestContainer struct {
 	TransactionService transaction.Provider
 	PartnerService     partner.Provider
 	UserService        user.Provider
+	PaymentProcessor   payment_processor.Provider
 }
 
 func NewPackageIntegrationTestContainer() TestContainer {
 	container := TestContainer{}
-<<<<<<< HEAD
-=======
-	container.UserService = user.NewTestContainer().UserService
->>>>>>> c481e966 (init handle multiple payment gateway)
 	container.PartnerService = partner.NewIntegrationTestContainer().PartnerService
 	container.TransactionService = transaction.NewPackageIntegrationTestContainer().TransactionService
 	container.PaymentService = NewPaymentService(
 		container.PartnerService,
-<<<<<<< HEAD
-		container.TransactionService)
-=======
 		container.TransactionService,
-	)
->>>>>>> c481e966 (init handle multiple payment gateway)
+		container.PaymentProcessor)
 	return container
 
 }
