8765535b91ccfad0aa542412a3d18e34c8f3cbc9
natnael jemaneh <njemaneh@chegebeya.com>
2025-04-24 09:42:37 +0300

    + resolve di issues

--- internal/adapter/primary/rest/handler/application/payment.go
+++ internal/adapter/primary/rest/handler/application/payment.go
@@ -24,7 +24,7 @@ func InitPayment() {
 }
 
 func (p *Payment) Init(applicationServices *application_core.Container, domainService *domain_core.Container) error {
-	p.service = applicationServices.PaymentService
+	p.service = domainService.PaymentService
 	return nil
 }
 
