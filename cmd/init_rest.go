fa7f695fbfab1aeba7552eac9d0d8f49a6e86178
expired-pineapple <ruthtirusew944@gmail.com>
2025-04-28 08:14:12 +0300

    =init cors

--- cmd/init_rest.go
+++ cmd/init_rest.go
@@ -19,5 +19,20 @@ func InitREST(mux *http.ServeMux, db *sql.DB, applicationServices *application_c
 	if err != nil {
 		panic(ErrFailedToBuildRoutes)
 	}
-	return mux
+
+	corsMux := http.NewServeMux()
+	corsMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		mux.ServeHTTP(w, r)
+	})
+
+	return corsMux
 }
