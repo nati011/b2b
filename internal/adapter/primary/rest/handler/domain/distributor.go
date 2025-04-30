24332662fa3918f388046e3c062cb37ba43ae375
expired-pineapple <ruthtirusew944@gmail.com>
2025-04-30 18:28:22 +0300

    - middleware from fetch distributor

--- internal/adapter/primary/rest/handler/domain/distributor.go
+++ internal/adapter/primary/rest/handler/domain/distributor.go
@@ -89,8 +89,8 @@ func (d *Distributor) Init(applicationServices *application_core.Container, doma
 }
 
 func (d *Distributor) Routes(mux *http.ServeMux) {
-	distributorHandler := http.HandlerFunc(d.GetDistributorHandler)
-	mux.Handle("GET /api/v1/distributor", d.middleware.Authenticate(distributorHandler))
+
+	mux.HandleFunc("GET /api/v1/distributor", d.GetDistributorHandler)
 	mux.HandleFunc("POST /api/v1/distributor", d.CreateDistributorHandler)
 	mux.HandleFunc("PUT /api/v1/distributor", d.UpdateDistributorHandler)
 
