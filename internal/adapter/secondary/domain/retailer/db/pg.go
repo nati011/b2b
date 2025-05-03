3de4a1f136ff82a85e5e243286c5906bb8fdb9b0
natnael jemaneh <njemaneh@chegebeya.com>
2025-05-03 11:22:02 +0300

    + resolve typing issue on retailer

--- internal/adapter/secondary/domain/retailer/db/pg.go
+++ internal/adapter/secondary/domain/retailer/db/pg.go
@@ -175,14 +175,14 @@ func (r *Postgres) GetAll(ctx context.Context) (port.GetAllResponse, error) {
 	// convert
 	for _, res := range result {
 		responseBase := port.GetResponse{
-			Id:          *res[0].(*int),
-			Name:        *res[1].(*string),
-			Tin:         *res[2].(*string),
-			Latitude:    *res[3].(*string),
-			Longitude:   *res[4].(*string),
-			GeneralZone: *res[5].(*string),
-			Region:      *res[6].(*string),
-			Woreda:      *res[7].(*string),
+			Id:          int(res[0].(int64)),
+			Name:        res[1].(string),
+			Tin:         res[2].(string),
+			Latitude:    res[3].(string),
+			Longitude:   res[4].(string),
+			GeneralZone: res[5].(string),
+			Region:      res[6].(string),
+			Woreda:      res[7].(string),
 		}
 		response.List = append(response.List, responseBase)
 	}
@@ -218,14 +218,14 @@ func (r *Postgres) GetByName(ctx context.Context, name string) (port.GetAllRespo
 	// convert
 	for _, res := range result {
 		responseBase := port.GetResponse{
-			Id:          *res[0].(*int),
-			Name:        *res[1].(*string),
-			Tin:         *res[2].(*string),
-			Latitude:    *res[3].(*string),
-			Longitude:   *res[4].(*string),
-			GeneralZone: *res[5].(*string),
-			Region:      *res[6].(*string),
-			Woreda:      *res[7].(*string),
+			Id:          int(res[0].(int64)),
+			Name:        res[1].(string),
+			Tin:         res[2].(string),
+			Latitude:    res[3].(string),
+			Longitude:   res[4].(string),
+			GeneralZone: res[5].(string),
+			Region:      res[6].(string),
+			Woreda:      res[7].(string),
 		}
 		response.List = append(response.List, responseBase)
 	}
@@ -289,7 +289,7 @@ func (r *Postgres) GetAllUserAgents(ctx context.Context, id int) (port.GetAllUse
 
 	for _, res := range result {
 		responseBase := port.GetUserResponse{
-			Id: *res[0].(*int),
+			Id: int(res[0].(int64)),
 		}
 		response.List = append(response.List, responseBase)
 	}
