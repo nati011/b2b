eaec740577f7a2a7743218527ec03fd15c873796
natnael jemaneh <njemaneh@chegebeya.com>
2025-05-07 17:18:44 +0300

    + extract catalogue into a separate module

--- internal/adapter/primary/rest/handler/domain/catalogue.go
+++ internal/adapter/primary/rest/handler/domain/catalogue.go
@@ -7,8 +7,7 @@ import (
 	util "b2b.nati011.github.com/internal/adapter/primary/rest/handler/util"
 	application_core "b2b.nati011.github.com/internal/core/application"
 	domain_core "b2b.nati011.github.com/internal/core/domain"
-	"b2b.nati011.github.com/internal/core/domain/configurable_product"
-	"b2b.nati011.github.com/internal/core/domain/product"
+	"b2b.nati011.github.com/internal/core/domain/catalogue"
 )
 
 type Image struct {
@@ -44,178 +43,60 @@ type GetCatalogueResponse struct {
 	Configurables          []CatalogueResponse                                  `json:"configurables"`
 }
 
+type ProductResponse struct {
+	Id             int               `json:"id"`
+	Name           string            `json:"name"`
+	Desc           string            `json:"desc"`
+	Price          float64           `json:"price"`
+	Stock          int               `json:"stock"`
+	AvailableStock int               `json:"available_stock"`
+	ReservedStock  int               `json:"reserved_stock"`
+	ExternalID     string            `json:"external_id"`
+	Attributes     map[string]string `json:"attributes"`
+	Images         []Image           `json:"images"`
+	DistributorId  int               `json:"distributor_id"`
+	CategoryId     []int             `json:"categories"`
+	IsActive       bool              `json:"is_active"`
+}
+
+type GetProductResponse struct {
+	Name                   string                                      `json:"name"`
+	Desc                   string                                      `json:"desc"`
+	IsActive               bool                                        `json:"is_active"`
+	Images                 []string                                    `json:"images"`
+	ConfigurableAttributes map[string][]ConfigurableAttributesResponse `json:"configurable_attributes"`
+	Configurables          []ProductResponse                           `json:"configurables"`
+}
+
 type GetAllCatalogueResponse struct {
 	List []GetProductResponse `json:"products"`
 }
 
 type Catalogue struct {
-	service                    product.Provider
-	configurableProductservice configurable_product.Provider
+	service catalogue.Provider
 }
 
 func InitCatalogue() {
 	handler.Register(new(Catalogue))
 }
 
-func (r *Catalogue) Init(applicationServices *application_core.Container, domainService *domain_core.Container) error {
-	r.service = domainService.ProductService
-	r.configurableProductservice = domainService.ConfigurableProductService
+func (c *Catalogue) Init(applicationServices *application_core.Container, domainService *domain_core.Container) error {
+	c.service = domainService.CatalogueService
 	return nil
 }
 
-func (p *Catalogue) Routes(mux *http.ServeMux) {
-	mux.HandleFunc("GET /api/v1/catalogue", p.GetCatalogueHandler)
+func (c *Catalogue) Routes(mux *http.ServeMux) {
+	mux.HandleFunc("GET /api/v1/catalogue", c.GetCatalogueHandler)
 }
 
-func (p *Catalogue) GetCatalogueHandler(w http.ResponseWriter, r *http.Request) {
-	// configurable products
-	var resp []GetCatalogueResponse
-	products_belonging_to_cps := []int{}
-	cp, err := p.configurableProductservice.GetAll(r.Context())
+func (c *Catalogue) GetCatalogueHandler(w http.ResponseWriter, r *http.Request) {
+	resp, err := c.service.GetAll(r.Context())
 	if err != nil {
 		switch err {
-		case configurable_product.ErrEmptyGetContent:
 		default:
 			util.ServerErrorResponse(w, err)
 			return
 		}
 	}
-
-	for _, j := range cp.List {
-		var configurables []CatalogueResponse
-		var configurableAttribute = make(map[string][]CatalogueConfigurableAttributesResponse)
-		for _, i := range j.Products {
-			//add product to cps belonging to products
-			products_belonging_to_cps = append(products_belonging_to_cps, i)
-			resp, err := p.service.Get(r.Context(), i)
-			if err != nil {
-				switch err {
-				case product.ErrIdNotFound:
-				default:
-					util.ServerErrorResponse(w, err)
-				}
-			}
-			var images []Image
-			for _, value := range resp.Images {
-				image := Image{
-					ImageUrl: value.ImageUrl,
-					BlurHash: value.BlurHash,
-				}
-				images = append(images, image)
-			}
-
-			configurables = append(configurables, CatalogueResponse{
-				Id:            resp.Id,
-				Name:          resp.Name,
-				Desc:          resp.Desc,
-				ExternalID:    resp.ExternalID,
-				Images:        images,
-				Price:         resp.Price,
-				Attributes:    resp.Attributes,
-				DistributorId: resp.DistributorId,
-				CategoryId:    resp.CategoryId,
-				Stock:         resp.Stock,
-				IsActive:      resp.IsActive,
-			})
-			for _, key := range j.Attributes {
-				configurableAttribute[key] = append(configurableAttribute[key], CatalogueConfigurableAttributesResponse{
-					ProductId:      resp.Id,
-					AttributeValue: resp.Attributes[key],
-				})
-
-			}
-		}
-		var images []Image
-		for _, value := range j.Images {
-			image := Image{
-				ImageUrl: value.ImageUrl,
-				BlurHash: value.BlurHash,
-			}
-			images = append(images, image)
-		}
-
-		resp = append(resp, GetCatalogueResponse{
-			Name:                   j.Name,
-			Desc:                   j.Desc,
-			IsActive:               j.IsAvailable,
-			Images:                 images,
-			ConfigurableAttributes: configurableAttribute,
-			Configurables:          configurables,
-		})
-	}
-
-	// standalone products
-	pr, err := p.service.GetAll(r.Context())
-	if err != nil {
-		switch err {
-		case product.ErrEmptyGetContent:
-		default:
-			util.ServerErrorResponse(w, err)
-			return
-		}
-	}
-	var configurables []CatalogueResponse
-	for _, i := range pr.List {
-		if !productInSlice(products_belonging_to_cps, i.Id) {
-			var images []Image
-			for _, value := range i.Images {
-				image := Image{
-					ImageUrl: value.ImageUrl,
-					BlurHash: value.BlurHash,
-				}
-				images = append(images, image)
-			}
-			configurables = append(configurables, CatalogueResponse{
-				Id:            i.Id,
-				Name:          i.Name,
-				Desc:          i.Desc,
-				ExternalID:    i.ExternalID,
-				Images:        images,
-				Price:         i.Price,
-				Attributes:    i.Attributes,
-				DistributorId: i.DistributorId,
-				CategoryId:    i.CategoryId,
-				Stock:         i.Stock,
-				IsActive:      i.IsActive,
-			})
-
-			var configurableAttribute = make(map[string][]CatalogueConfigurableAttributesResponse)
-			for attr_key, attr_val := range i.Attributes {
-				configurableAttribute[attr_key] = []CatalogueConfigurableAttributesResponse{
-					{
-						ProductId:      i.Id,
-						AttributeValue: attr_val,
-					},
-				}
-			}
-			var imageCatalogue []Image
-			for _, value := range i.Images {
-				image := Image{
-					ImageUrl: value.ImageUrl,
-					BlurHash: value.BlurHash,
-				}
-				imageCatalogue = append(imageCatalogue, image)
-			}
-
-			resp = append(resp, GetCatalogueResponse{
-				Name:                   i.Name,
-				Desc:                   i.Desc,
-				IsActive:               i.IsActive,
-				Images:                 imageCatalogue,
-				ConfigurableAttributes: configurableAttribute,
-				Configurables:          configurables,
-			})
-		}
-	}
-
 	util.OperationSuccessResponse(w, util.Envelope{"products": resp})
-}
-
-func productInSlice(productsSlice []int, value int) bool {
-	for _, v := range productsSlice {
-		if v == value {
-			return true
-		}
-	}
-	return false
 }
