50a37c47afa0f03590fe21a43eefbaf53a559a75
natnael jemaneh <njemaneh@chegebeya.com>
2025-05-07 18:44:20 +0300

    + passing int tests

---
+++ internal/core/application/payment_verification/test/integration/payment_partner/payment_partner_test.go
@@ -0,0 +1,67 @@
+package paymentpartner
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"b2b.nati011.github.com/internal/core/application/payment_partner"
+	"b2b.nati011.github.com/internal/core/application/payment_verification"
+	"b2b.nati011.github.com/internal/core/application/payment_verification/test"
+)
+
+var testContainer test.TestContainer
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	os.Exit(code)
+}
+
+func setup() {
+	testContainer = test.NewPackageIntegrationTestContainer()
+}
+
+func Test_DisallowVerificationIfPaymentProviderIsInactive(t *testing.T) {
+	ctx := context.Background()
+	var err error
+	PaymentPartnerId, err := testContainer.PartnerService.Create(ctx,
+		&payment_partner.CreateRequest{
+			Name:    "chapa",
+			Icon:    "test",
+			BaseURL: "https://api.chapa.co",
+			Secret:  "CHASECK_TEST-KUZmLnnAPtwFg8hQPqCx7mc4o7TUbIe5",
+		})
+
+	if err != nil {
+		panic("failed to create order partner")
+	}
+	_, err = testContainer.PaymementVerificationService.Verify(ctx, PaymentPartnerId, "txRef")
+	wantErr := payment_verification.ErrPaymentPartnerNotSupported
+	if err != wantErr {
+		t.Errorf("Expected err: %v Got: %v", wantErr, err)
+	}
+}
+
+func Test_AllowVerificationIfPaymentProviderIsActive(t *testing.T) {
+	ctx := context.Background()
+	PaymentPartnerId, err := testContainer.PartnerService.Create(ctx,
+		&payment_partner.CreateRequest{
+			Name:    "chapa",
+			Icon:    "test",
+			BaseURL: "https://api.chapa.co",
+			Secret:  "CHASECK_TEST-KUZmLnnAPtwFg8hQPqCx7mc4o7TUbIe5",
+		})
+	if err != nil {
+		t.Fatalf("Failed to create payment partner: %v", err)
+	}
+	//activate
+	err = testContainer.PartnerService.Activate(ctx, PaymentPartnerId)
+	if err != nil {
+		t.Fatalf("Failed to activate payment partner: %v", err)
+	}
+	_, err = testContainer.PaymementVerificationService.Verify(ctx, PaymentPartnerId, "txRef")
+	if err != nil {
+		t.Fatalf("Failed to checkout: %v", err)
+	}
+}
