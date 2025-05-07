884c85e8d5970d512102e3e6734aa30b56510802
Ruth Tirusew <87856799+ruth-tirusew@users.noreply.github.com>
2025-05-07 18:45:02 +0300

    Update init_db.go

--- cmd/init_db.go
+++ cmd/init_db.go
@@ -28,23 +28,23 @@ func InitDB(connectionString string, file_location string) *sql.DB {
 	}
 
 	// ddl
-	err = runMigration(db, file_location+`/core_db.sql`)
+	// err = runMigration(db, file_location+`/core_db.sql`)
 
-	if err != nil {
-		log.Fatalf("Error running ddl migration: %v", err)
-	}
+	// if err != nil {
+	// 	log.Fatalf("Error running ddl migration: %v", err)
+	// }
 
-	// functions
-	err = runMigration(db, file_location+"/core_db_functions.sql")
-	if err != nil {
-		log.Fatalf("Error running function migration: %v", err)
-	}
+	// // functions
+	// err = runMigration(db, file_location+"/core_db_functions.sql")
+	// if err != nil {
+	// 	log.Fatalf("Error running function migration: %v", err)
+	// }
 
-	// seed
-	err = runMigration(db, file_location+"/core_init_migration_script.sql")
-	if err != nil {
-		log.Fatalf("Error running seed migration: %v", err)
-	}
+	// // seed
+	// err = runMigration(db, file_location+"/core_init_migration_script.sql")
+	// if err != nil {
+	// 	log.Fatalf("Error running seed migration: %v", err)
+	// }
 
 	return db
 }
