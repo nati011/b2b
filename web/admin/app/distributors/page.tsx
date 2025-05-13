6d50ef002b5b2fcf600fd24a2bdfa7d0fd40ba33
expired-pineapple <ruthtirusew944@gmail.com>
2025-05-13 18:39:28 +0300

    fix: data type on distributor location

--- web/admin/app/distributors/page.tsx
+++ web/admin/app/distributors/page.tsx
@@ -1,10 +1,12 @@
 'use client'
-import TableLayout from "@/app/components/table-layout";
-import { columns } from "@/app/distributors/column"
 import useDistributorsStore from "@/app/libs/store/useDistributorStore"
 import { useEffect } from "react";
 import Heading from "../components/breadcrumb";
 import { DataTable } from "@/components/ui/datatable";
+import { ColumnDef } from "@tanstack/react-table";
+import { Distributor } from '@/app/libs/types';
+import Link from "next/link";
+import { GoEye } from "react-icons/go";
 
 export default function Distributors() {
   const {
@@ -24,7 +26,45 @@ export default function Distributors() {
       "href": `/distributors`,
     }
   ]
-
+  const columns: ColumnDef<Distributor>[] = [
+    {
+      accessorKey: "id",
+      header: "Id",
+    },
+    {
+      accessorKey: "name",
+      header: "Name",
+    },
+    {
+      accessorKey: "tin",
+      header: "Tin",
+    },
+    {
+      accessorKey: "general_zone",
+      header: "General Zone",
+    },
+    {
+      accessorKey: "region",
+      header: "Region",
+    },
+    {
+      accessorKey: "woreda",
+      header: "Woreda",
+    },
+    {
+      id: "actions",
+      enableHiding: false,
+      cell: ({ row }) => {
+        return (
+          <div className="flex items-center gap-2 text-gray-900">
+            <Link href={`/distributor/${row.original.id}`}>
+              <GoEye />
+            </Link>
+          </div>
+        );
+      },
+    },
+  ];
 
 
   return (
