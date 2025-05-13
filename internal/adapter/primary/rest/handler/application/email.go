840dc02c8c1ea5df4dda609addad23fb9b20becb
natnael jemaneh <njemaneh@chegebeya.com>
2025-05-13 18:22:13 +0300

    + send message endpoint done

---
+++ internal/adapter/primary/rest/handler/application/email.go
@@ -0,0 +1,71 @@
+package application
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"b2b.nati011.github.com/internal/adapter/primary/rest/handler"
+	util "b2b.nati011.github.com/internal/adapter/primary/rest/handler/util"
+	application_core "b2b.nati011.github.com/internal/core/application"
+	"b2b.nati011.github.com/internal/core/application/email"
+	"b2b.nati011.github.com/internal/core/application/template"
+	domain_core "b2b.nati011.github.com/internal/core/domain"
+)
+
+type SendEmailRequest struct {
+	To         string            `json:"to"`
+	Subject    string            `json:"subject"`
+	TemplateId int               `json:"template_id"`
+	Args       map[string]string `json:"args"`
+	ExternalId string            `json:"ext_id"`
+}
+
+type Email struct {
+	Service email.Provider
+}
+
+func InitEmail() {
+	handler.Register(new(Email))
+}
+
+func (e *Email) Init(applicationServices *application_core.Container, domainService *domain_core.Container) error {
+	e.Service = applicationServices.EmailService
+	return nil
+}
+
+func (e *Email) Routes(mux *http.ServeMux) {
+	mux.HandleFunc("POST /api/v1/email", e.SendEmailHandler)
+}
+
+func (e *Email) SendEmailHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		util.RequestErrorResponse(w, err)
+		return
+	}
+	defer r.Body.Close()
+	var requestBody SendEmailRequest
+	if err := json.Unmarshal(body, &requestBody); err != nil {
+		util.RequestErrorResponse(w, err)
+		return
+	}
+
+	err = e.Service.Send(&email.SendRequest{
+		To:         requestBody.To,
+		Subject:    requestBody.Subject,
+		TemplateId: requestBody.TemplateId,
+		Args:       requestBody.Args,
+		ExternalId: requestBody.ExternalId})
+	if err != nil {
+		switch err {
+		case template.ErrUnknown:
+			util.ServerErrorResponse(w, err)
+			return
+		default:
+			util.RequestErrorResponse(w, err)
+			return
+		}
+	}
+	util.OperationSuccessMessageResponse(w, "email sent successfuly!")
+}
