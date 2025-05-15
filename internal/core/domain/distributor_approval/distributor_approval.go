a877ba3347c24df2ab66cc9098ffec520822ab6e
natnael jemaneh <njemaneh@chegebeya.com>
2025-05-15 19:41:57 +0300

    + passing unit tets

--- internal/core/domain/distributor_approval/distributor_approval.go
+++ internal/core/domain/distributor_approval/distributor_approval.go
@@ -2,11 +2,21 @@ package distributorApproval
 
 import (
 	"context"
+	"errors"
 
+	port_commons "b2b.nati011.github.com/internal/port/commons/db"
 	port "b2b.nati011.github.com/internal/port/domain/distributor_approval"
 )
 
-type ApproveRequest struct {
+var (
+	ErrAlreadyApproved  = errors.New("oopsy, already approved")
+	ErrAlreadyRejected  = errors.New("oopsy, already rejected")
+	ErrCommentMandatory = errors.New("oopsy, comment mandatory")
+	ErrUnknown          = errors.New("oopsy, unknown error")
+	ErrNoReview         = errors.New("oopsy, no reviews")
+)
+
+type ApprovalRequest struct {
 	DistributorId int
 }
 
@@ -16,8 +26,9 @@ type RejectionRequest struct {
 }
 
 type Provider interface {
-	Approve(ctx context.Context, id int) error
-	Reject(ctx context.Context, id int) error
+	Approve(ctx context.Context, req *ApprovalRequest) error
+	Reject(ctx context.Context, req *RejectionRequest) error
+	GetApprovalStatus(ctx context.Context, distributorId int) (bool, error)
 }
 
 type DistributorApprovalService struct {
@@ -30,10 +41,67 @@ func NewDistributorApprovalService(db port.DB) Provider {
 	}
 }
 
-func (d *DistributorApprovalService) Approve(ctx context.Context, id int) error {
+func (d *DistributorApprovalService) Approve(ctx context.Context, req *ApprovalRequest) error {
+	isApproved, err := d.DB.GetApprovalStatus(ctx, req.DistributorId)
+	if err != nil {
+		switch err {
+		case port_commons.ErrSysNoRows:
+		default:
+			return ErrUnknown
+		}
+	}
+	if isApproved {
+		return ErrAlreadyApproved
+	}
+	err = d.DB.Approve(ctx, port.ApprovalRequest{
+		DistributorId: req.DistributorId,
+	})
+	if err != nil {
+		return ErrUnknown
+	}
 	return nil
 }
 
-func (d *DistributorApprovalService) Reject(ctx context.Context, id int) error {
+func (d *DistributorApprovalService) Reject(ctx context.Context, req *RejectionRequest) error {
+	if req.Comment == "" {
+		return ErrCommentMandatory
+	}
+
+	isApproved, err := d.DB.GetApprovalStatus(ctx, req.DistributorId)
+	if err != nil {
+		switch err {
+		case port_commons.ErrSysNoRows:
+		default:
+			return ErrUnknown
+		}
+	} else {
+		if !isApproved {
+			return ErrAlreadyRejected
+		}
+	}
+
+	err = d.DB.Reject(ctx, port.RejectRequest{
+		DistributorId: req.DistributorId,
+		Comment:       req.Comment,
+	})
+	if err != nil {
+		return ErrUnknown
+	}
 	return nil
+}
+
+func (d *DistributorApprovalService) GetApprovalStatus(ctx context.Context, distributorId int) (bool, error) {
+	resp, err := d.DB.GetApprovalStatus(ctx, distributorId)
+	if err != nil {
+		switch err {
+		case port_commons.ErrSysNoRows:
+			return false, ErrNoReview
+		default:
+			return false, ErrUnknown
+		}
+	}
+	if resp {
+		return true, nil
+	}
+	return false, nil
 }
