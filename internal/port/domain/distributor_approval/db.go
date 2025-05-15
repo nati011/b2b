77b17699abf1377d2e0c532d4cb6ad57ca06baf5
natnael jemaneh <njemaneh@chegebeya.com>
2025-05-15 19:41:57 +0300

    + passing unit tets

--- internal/port/domain/distributor_approval/db.go
+++ internal/port/domain/distributor_approval/db.go
@@ -2,33 +2,41 @@ package distributorApproval
 
 import (
 	"context"
+	"time"
 )
 
 type GetVerdictResponse struct {
 	DistributorId int
 	Verdict       bool
 	Comment       string
+	ReviewedBy    string
+	ReviewedAt    time.Time
 }
 
 type GetAuditReportResponse struct {
 	List []GetVerdictResponse
 }
 
-type CreateResponse struct {
+type RejectRequest struct {
 	DistributorId int
-	Verdict       bool
+	Comment       string
+	ReviewedBy    string
+}
+
+type ApprovalRequest struct {
+	DistributorId int
 	Comment       string
 	ReviewedBy    string
 }
 
 type Reader interface {
-	GetVerdict(ctx context.Context, id int) (GetVerdictResponse, error)
-	GetAuditReport(ctx context.Context) (GetAllVerdictResponse, error)
+	GetApprovalStatus(ctx context.Context, distributorId int) (bool, error)
+	GetReviewReport(ctx context.Context, distributorId int) (GetAuditReportResponse, error)
 }
 
 type Writer interface {
-	Approve(ctx context.Context, id int) error
-	Reject(ctx context.Context, id int) error
+	Approve(ctx context.Context, req ApprovalRequest) error
+	Reject(ctx context.Context, req RejectRequest) error
 }
 
 type DB interface {
