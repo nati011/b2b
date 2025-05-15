4856b65c7cfcc98f0c1e89870ceea53977cfb0df
natnael jemaneh <njemaneh@chegebeya.com>
2025-05-15 19:41:57 +0300

    + passing unit tets

--- internal/core/domain/distributor_approval/testContainer.go
+++ internal/core/domain/distributor_approval/testContainer.go
@@ -1,1 +1,20 @@
 package distributorApproval
+
+import (
+	db_adapter "b2b.nati011.github.com/internal/adapter/secondary/domain/distributor_approval/db"
+)
+
+type TestContainer struct {
+	DistributorApprovalService Provider
+}
+
+func NewTestContainer() TestContainer {
+	container := TestContainer{}
+	container.DistributorApprovalService = NewDistributorApprovalService(db_adapter.NewMock())
+
+	return container
+}
+
+func (t *TestContainer) Teardown() {
+	t.DistributorApprovalService = NewDistributorApprovalService(db_adapter.NewMock())
+}
