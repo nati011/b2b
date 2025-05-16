296ce2f8695809e8d9a61cf04701124bd64d83ee
natnael jemaneh <njemaneh@chegebeya.com>
2025-05-16 13:09:47 +0300

    + init email fix

--- cmd/main.go
+++ cmd/main.go
@@ -51,7 +51,7 @@ func main() {
 	//for testing purposes
 	InitAuth(&cfg)
 
-	smtp_server := InitEmail(cfg.Email, cfg.SMTP, cfg.EmailPassword)
+	// InitEmail(cfg.Email, cfg.SMTP, cfg.EmailPassword)
 	// InitSMS(cfg.Email, cfg.SMTP)
 
 	application_constainer := application_core.NewContainer(
@@ -65,6 +65,7 @@ func main() {
 		cfg.KeycloakClientSecret,
 		cfg.Email,
 		cfg.SMTP,
+		cfg.EmailPassword,
 		cfg.MinMobileClientCompatibleVersion,
 		cfg.BaseUrl,
 		cfg.FrontendUrl,
