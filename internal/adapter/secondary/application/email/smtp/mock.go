ca862ae6a7cbb5fcc2d80bb8cd40ac597103b970
natnael jemaneh <njemaneh@chegebeya.com>
2025-05-16 11:52:13 +0300

    + pull out smtp to port

--- internal/adapter/secondary/application/email/smtp/mock.go
+++ internal/adapter/secondary/application/email/smtp/mock.go
@@ -1,9 +1,11 @@
 package email
 
+import port "b2b.nati011.github.com/internal/port/application/email"
+
 type Mock struct {
 }
 
-func (m *Mock) Send(r Request) error {
+func (m *Mock) Send(r port.Request) error {
 	return nil
 }
 
