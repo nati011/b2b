be9a061cf1b6f7c3a48ebf84846efd5ac64a65e3
natnael jemaneh <njemaneh@chegebeya.com>
2025-05-20 09:46:18 +0300

    + cleanup

--- cmd/init_rest.go
+++ cmd/init_rest.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"b2b.nati011.github.com/internal/adapter/primary/rest"
+	util "b2b.nati011.github.com/internal/adapter/primary/rest/handler/util"
 	application_core "b2b.nati011.github.com/internal/core/application"
 	domain_core "b2b.nati011.github.com/internal/core/domain"
 )
@@ -14,8 +15,8 @@ var (
 	ErrFailedToBuildRoutes = errors.New("oopsy, failed to build routes")
 )
 
-func InitREST(mux *http.ServeMux, db *sql.DB, applicationServices *application_core.Container, domainServices *domain_core.Container) *http.ServeMux {
-	err := rest.BuildRouter(mux, applicationServices, domainServices)
+func InitREST(authMiddleWare *util.AuthMiddleware, mux *http.ServeMux, db *sql.DB, applicationServices *application_core.Container, domainServices *domain_core.Container) *http.ServeMux {
+	err := rest.BuildRouter(authMiddleWare, mux, applicationServices, domainServices)
 	if err != nil {
 		panic(ErrFailedToBuildRoutes)
 	}
