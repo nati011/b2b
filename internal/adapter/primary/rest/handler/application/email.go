4e888dd106879bf8c377982356296e0576db5530
natnael jemaneh <njemaneh@chegebeya.com>
2025-05-20 19:00:35 +0300

    secure email

--- internal/adapter/primary/rest/handler/application/email.go
+++ internal/adapter/primary/rest/handler/application/email.go
@@ -22,7 +22,8 @@ type SendEmailRequest struct {
 }
 
 type Email struct {
-	Service email.Provider
+	authMiddleware util.AuthMiddleware
+	Service        email.Provider
 }
 
 func InitEmail() {
@@ -30,12 +31,15 @@ func InitEmail() {
 }
 
 func (e *Email) Init(authMiddleWare *util.AuthMiddleware, applicationServices *application_core.Container, domainService *domain_core.Container) error {
+	e.authMiddleware = *applicationServices.AuthMiddleware
 	e.Service = applicationServices.EmailService
 	return nil
 }
 
 func (e *Email) Routes(mux *http.ServeMux) {
-	mux.HandleFunc("POST /api/v1/email", e.SendEmailHandler)
+	mux.HandleFunc("POST /api/v1/email", func(w http.ResponseWriter, r *http.Request) {
+		e.authMiddleware.RequireAuthentication(http.HandlerFunc(e.SendEmailHandler)).ServeHTTP(w, r)
+	})
 }
 
 func (e *Email) SendEmailHandler(w http.ResponseWriter, r *http.Request) {
