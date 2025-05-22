4b0050e9c451b63f0661d0a4b3d08ebdcc6620e9
natnael jemaneh <njemaneh@chegebeya.com>
2025-05-22 14:54:48 +0300

    + wrap auth inside user

--- internal/adapter/primary/rest/handler/application/auth.go
+++ internal/adapter/primary/rest/handler/application/auth.go
@@ -34,63 +34,10 @@ func (a *AuthHandler) Init(authMiddleWare *middleware.Auth, services *applicatio
 }
 
 func (a *AuthHandler) Routes(mux *http.ServeMux) {
-	mux.HandleFunc("POST /api/v1/auth/login", func(w http.ResponseWriter, r *http.Request) {
-		a.authMiddleware.RequireNoAuthentication(http.HandlerFunc(a.LoginHandler)).ServeHTTP(w, r)
-	})
-
-	mux.HandleFunc("POST /api/v1/auth/refresh", func(w http.ResponseWriter, r *http.Request) {
-		a.authMiddleware.RequireNoAuthentication(http.HandlerFunc(a.RefreshTokenHandler)).ServeHTTP(w, r)
-	})
 
 	mux.HandleFunc("POST /api/v1/auth/reset/{token}", func(w http.ResponseWriter, r *http.Request) {
 		a.authMiddleware.RequireNoAuthentication(http.HandlerFunc(a.ResetCredentialsHandler)).ServeHTTP(w, r)
 	})
-}
-
-func (h *AuthHandler) LoginHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		util.RequestErrorResponse(w, err)
-		return
-	}
-	defer r.Body.Close()
-
-	var requestBody auth.LoginUserRequest
-	if err := json.Unmarshal(body, &requestBody); err != nil {
-		util.RequestErrorResponse(w, err)
-		return
-	}
-
-	loginResponse, err := h.service.ClientLogin(r.Context(), requestBody)
-	if err != nil {
-		switch err {
-		case auth.ErrUnknown:
-			util.ServerErrorResponse(w, err)
-		default:
-			util.UnauthorizedResponse(w)
-			return
-		}
-	}
-	util.OperationSuccessResponse(w, loginResponse.JWT)
-
-}
-
-func (h *AuthHandler) RefreshTokenHandler(w http.ResponseWriter, r *http.Request) {
-	var req auth.RefreshTokenRequest
-
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
-		return
-	}
-
-	refreshResponse, err := h.service.RefreshToken(r.Context(), req)
-	if err != nil {
-		util.RequestErrorResponse(w, err)
-		return
-	}
-	defer r.Body.Close()
-
-	util.OperationSuccessResponse(w, util.Envelope{"body": refreshResponse})
 }
 
 func (h *AuthHandler) ResetCredentialsHandler(w http.ResponseWriter, r *http.Request) {
