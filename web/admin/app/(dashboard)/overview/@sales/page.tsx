6b75ebdb05ee2eaec95f1a37b200e9bc6fae5cf3
expired-pineapple <ruthtirusew944@gmail.com>
2025-05-22 10:09:42 +0300

    init ui change for dashboard

---
+++ web/admin/app/(dashboard)/overview/@sales/page.tsx
@@ -0,0 +1,65 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardTitle,
+  CardDescription
+} from '@/components/ui/card';
+
+const salesData = [
+  {
+    name: 'Olivia Martin',
+    email: 'olivia.martin@email.com',
+    avatar: 'https://api.slingacademy.com/public/sample-users/1.png',
+    fallback: 'OM',
+    amount: '1Item'
+  },
+  {
+    name: 'Jackson Lee',
+    email: 'jackson.lee@email.com',
+    avatar: 'https://api.slingacademy.com/public/sample-users/2.png',
+    fallback: 'JL',
+    amount: '3 Item'
+  },
+  {
+    name: 'Isabella Nguyen',
+    email: 'isabella.nguyen@email.com',
+    avatar: 'https://api.slingacademy.com/public/sample-users/3.png',
+    fallback: 'IN',
+    amount: '4 Item'
+  },
+  {
+    name: 'William Kim',
+    email: 'will@email.com',
+    avatar: 'https://api.slingacademy.com/public/sample-users/4.png',
+    fallback: 'WK',
+    amount: '2 Items'
+  },]
+
+export default function RecentSales() {
+  return (
+    <Card className='h-full  rounded-sm shadow-none'>
+      <CardHeader>
+        <CardTitle>Recent Sales</CardTitle>
+        <CardDescription>You made 265 sales this month.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className='space-y-8'>
+          {salesData.map((sale, index) => (
+            <div key={index} className='flex items-center'>
+              <Avatar className='h-10 w-10'>
+                <AvatarFallback>{sale.fallback}</AvatarFallback>
+              </Avatar>
+              <div className='ml-4 space-y-1'>
+                <p className='text-sm leading-none font-medium'>{sale.name}</p>
+                <div className='ml-auto font-medium text-sm text-gray-700'>{sale.amount}</div>
+              </div>
+              <div className='ml-auto teext-sm inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-amber-100/50'>Pending</div>
+            </div>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
