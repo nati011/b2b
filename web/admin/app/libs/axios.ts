a996232803b4e4bdf6ab9b6dd127e26c2c49cf7b
expired-pineapple <ruthtirusew944@gmail.com>
2025-05-24 01:42:20 +0300

    init product pagination

--- web/admin/app/libs/axios.ts
+++ web/admin/app/libs/axios.ts
@@ -1,5 +1,6 @@
 import { getSession } from "@/actions/getSession";
 import axios from "axios";
+import { signIn } from "next-auth/react";
 
 const apiUrl = "http://localhost:3000/api/v1";
 
@@ -10,6 +11,11 @@ const axiosIns = axios.create({
 axiosIns.interceptors.request.use(
   async (config) => {
     const session = await getSession()
+    // @ts-ignore
+    if (session?.error === "RefreshAccessTokenError") {
+      window.location.href = "/auth/signin"
+    }
+
     // @ts-ignore
     if (session && session?.accessToken) {
       // @ts-ignore
