e17e46076e81e6b3785820daf07248baab3a1342
natnael jemaneh <njemaneh@chegebeya.com>
2025-05-23 10:36:07 +0300

    + cleanup

--- internal/core/domain/retailer/test/testContainer.go
+++ internal/core/domain/retailer/test/testContainer.go
@@ -17,18 +17,16 @@ type TestContainer struct {
 func NewDBIntegrationTestContainer(db *sql.DB) TestContainer {
 	container := TestContainer{}
 	container.UserService = user.NewIntegrationTestContainer(db).UserService
-	container.RetailerService = retailer.NewRetailerService(container.UserService, db_adapter.NewPostgres(db, &config.Pagination{
-		Limit:  10,
-		Offset: 0,
-	}))
+	container.RetailerService = retailer.NewRetailerService(
+		container.UserService,
+		db_adapter.NewPostgres(db, config.DefaultPaginationBuilder().Build()))
 
 	return container
 }
 
 func (t *TestContainer) Teardown(db *sql.DB) {
 	t.UserService = user.NewIntegrationTestContainer(db).UserService
-	t.RetailerService = retailer.NewRetailerService(t.UserService, db_adapter.NewPostgres(db, &config.Pagination{
-		Limit:  10,
-		Offset: 0,
-	}))
+	t.RetailerService = retailer.NewRetailerService(
+		t.UserService,
+		db_adapter.NewPostgres(db, config.DefaultPaginationBuilder().Build()))
 }
