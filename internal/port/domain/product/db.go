6207933d4fbdf13ba9f8e6f16da72e3f0c3bddaf
expired-pineapple <ruthtirusew944@gmail.com>
2025-05-26 19:24:07 +0300

    + product search passing tests

--- internal/port/domain/product/db.go
+++ internal/port/domain/product/db.go
@@ -133,6 +133,7 @@ type FreeReservedRequest struct {
 type Reader interface {
 	Get(ctx context.Context, id int) (GetResponse, error)
 	GetAll(ctx context.Context) (GetAllResponse, error)
+	Search(ctx context.Context, search_query string) (GetAllResponse, error)
 	GetByName(ctx context.Context, req *GetByNameRequest) (GetAllResponse, error)
 	GetByExternalId(ctx context.Context, req *GetByExternalIdRequest) (GetAllResponse, error)
 	GetByDistributorId(ctx context.Context, req *GetByDistributorIdRequest) (GetAllResponse, error)
