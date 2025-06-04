fa2efdd6d192126cf76809eb4c074b725a6fd0ce
natnael jemaneh <njemaneh@chegebeya.com>
2025-06-04 11:51:51 +0300

    + add init reset token endpoint

--- internal/adapter/primary/rest/handler/application/auth.go
+++ internal/adapter/primary/rest/handler/application/auth.go
@@ -23,12 +23,19 @@ type ResetPasswordRequest struct {
 	NewPassword string `json:"password"`
 }
 
+type InitResetPasswordRequest struct {
+	UserId string `json:"user_id"`
+	Email  string `json:"email"`
+}
+
 func InitAuth() {
 	handler.Register(new(AuthHandler))
 
 	handler.RegisterResource("/api/v1/auth/login")
+	handler.RegisterResource("/api/v1/auth/logout")
 	handler.RegisterResource("/api/v1/auth/refresh")
 	handler.RegisterResource("/api/v1/auth/reset/{token}")
+	handler.RegisterResource("/api/v1/auth/init_reset")
 }
 
 func (a *AuthHandler) Init(authMiddleWare *middleware.Auth, services *application_core.Container, domainService *domain_core.Container) error {
@@ -51,9 +58,35 @@ func (a *AuthHandler) Routes(mux *http.ServeMux) {
 		a.authMiddleware.RequireNoAuthentication(http.HandlerFunc(a.RefreshTokenHandler)).ServeHTTP(w, r)
 	})
 
+	mux.HandleFunc("POST /api/v1/auth/init_reset", func(w http.ResponseWriter, r *http.Request) {
+		a.authMiddleware.RequireNoAuthentication(http.HandlerFunc(a.InitResetTokenHandler)).ServeHTTP(w, r)
+	})
+
 	mux.HandleFunc("POST /api/v1/auth/reset/{token}", func(w http.ResponseWriter, r *http.Request) {
 		a.authMiddleware.RequireNoAuthentication(http.HandlerFunc(a.ResetCredentialsHandler)).ServeHTTP(w, r)
 	})
+}
+
+func (h *AuthHandler) InitResetTokenHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		util.RequestErrorResponse(w, err)
+		return
+	}
+	defer r.Body.Close()
+	var requestBody InitResetPasswordRequest
+	if err := json.Unmarshal(body, &requestBody); err != nil {
+		util.RequestErrorResponse(w, err)
+		return
+	}
+	err = h.service.InitClientCredentialsReset(r.Context(), auth.InitClientCredentialsResetRequest{
+		UserId: requestBody.UserId,
+		Email:  requestBody.Email})
+	if err != nil {
+		util.RequestErrorResponse(w, err)
+		return
+	}
+	util.OperationSuccessMessageResponse(w, "password reset init successfully")
 }
 
 func (h *AuthHandler) ResetCredentialsHandler(w http.ResponseWriter, r *http.Request) {
