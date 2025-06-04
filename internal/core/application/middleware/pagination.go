d702588b215150197092e56e86bfb2b72887a2ac
natnael jemaneh <njemaneh@chegebeya.com>
2025-06-04 11:28:09 +0300

    + fix retailer fetch issue

--- internal/core/application/middleware/pagination.go
+++ internal/core/application/middleware/pagination.go
@@ -31,6 +31,10 @@ func (p *PaginationMiddleware) Paginate(next http.Handler) http.Handler {
 		var offset int
 		var err error
 
+		if (paramLimitValue == "") && (paramOffsetValue == "") {
+			next.ServeHTTP(w, r)
+		}
+
 		if paramLimitValue != "" {
 			limit, err = strconv.Atoi(paramLimitValue)
 			if err != nil {
