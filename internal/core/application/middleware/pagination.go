ed0401eafb7f0ab7814e8f46138cbf9efb13226a
natnael jemaneh <njemaneh@chegebeya.com>
2025-06-04 14:19:30 +0300

    + terminate paginate after empty check

--- internal/core/application/middleware/pagination.go
+++ internal/core/application/middleware/pagination.go
@@ -31,8 +31,9 @@ func (p *PaginationMiddleware) Paginate(next http.Handler) http.Handler {
 		var offset int
 		var err error
 
-		if (paramLimitValue == "") && (paramOffsetValue == "") {
+		if paramLimitValue == "" && paramOffsetValue == "" {
 			next.ServeHTTP(w, r)
+			return
 		}
 
 		if paramLimitValue != "" {
