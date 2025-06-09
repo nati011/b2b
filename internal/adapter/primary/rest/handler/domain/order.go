d38f3575a98e6843bfa9fb7bfa2a481b9792e7d7
natnael jemaneh <njemaneh@chegebeya.com>
2025-06-09 15:51:52 +0300

    + init response fix

--- internal/adapter/primary/rest/handler/domain/order.go
+++ internal/adapter/primary/rest/handler/domain/order.go
@@ -238,7 +238,7 @@ func (o *Order) InitPaymentHandler(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 		}
-		util.OperationSuccessResponse(w, util.Envelope{"order": init_resp})
+		util.OperationSuccessResponse(w, init_resp)
 	}
 }
 
