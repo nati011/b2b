6b5887666d45778d9ba2fcd536bf775508bb3a32
natnael jemaneh <njemaneh@chegebeya.com>
2025-06-10 09:41:29 +0300

    + distributor validation added

--- internal/core/domain/product/testContainer.go
+++ internal/core/domain/product/testContainer.go
@@ -4,11 +4,13 @@ import (
 	category_db "b2b.nati011.github.com/internal/adapter/secondary/domain/category/db"
 	db "b2b.nati011.github.com/internal/adapter/secondary/domain/product/db"
 	category "b2b.nati011.github.com/internal/core/domain/category"
+	"b2b.nati011.github.com/internal/core/domain/distributor"
 )
 
 type TestContainer struct {
-	CategoryService category.Provider
-	ProductService  Provider
+	CategoryService    category.Provider
+	ProductService     Provider
+	DistributorService distributor.Provider
 }
 
 func NewPackageIntegrationTestContainer() TestContainer {
@@ -16,10 +18,11 @@ func NewPackageIntegrationTestContainer() TestContainer {
 	container.CategoryService = category.NewCategory(
 		category_db.NewMock(),
 	)
-
+	container.DistributorService = distributor.NewPackageIntegrationTestContainer().DistributorService
 	container.ProductService = NewProduct(
 		db.NewMock(),
 		container.CategoryService,
+		container.DistributorService,
 	)
 	return container
 }
@@ -28,5 +31,6 @@ func (t *TestContainer) Teardown() {
 	t.ProductService = NewProduct(
 		db.NewMock(),
 		t.CategoryService,
+		t.DistributorService,
 	)
 }
