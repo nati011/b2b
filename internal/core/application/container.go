cc11c395211b10839ea77a52518b8174ce86a3d4
natnael jemaneh <njemaneh@chegebeya.com>
2025-06-13 15:55:59 +0300

    + add role handler for user

--- internal/core/application/container.go
+++ internal/core/application/container.go
@@ -105,7 +105,6 @@ func NewContainer(
 	container.InitTransactionService()
 	container.InitResourceService()
 	container.InitRoleService()
-	container.InitUserService()
 	container.InitAuthService(
 		cfg.KeycloakInstanceURL,
 		cfg.KeycloakUsername,
@@ -114,6 +113,7 @@ func NewContainer(
 		cfg.KeycloakApplicationRealm,
 		cfg.KeycloakClientId,
 		cfg.KeycloakClientSecret)
+	container.InitUserService()
 	container.InitPaymentService()
 	container.InitCheckoutService(cfg.BaseUrl, cfg.FrontendUrl)
 	container.InitAuthMiddleware()
