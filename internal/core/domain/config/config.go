8349fdda02745b9088d0f4cb87019f92ffe31adb
natnael jemaneh <njemaneh@chegebeya.com>
2025-06-20 11:20:09 +0300

    + cleanup

--- internal/core/domain/config/config.go
+++ internal/core/domain/config/config.go
@@ -23,7 +23,7 @@ type SetOrderExpiryRequest struct {
 type Provider interface {
 	SetDefaults(ctx context.Context) error
 
-	//order expiry
+	// order expiry
 	GetOrderExpiryConfig(ctx context.Context) (GetOrderExpiryResponse, error)
 	SetOrderExpiryConfig(ctx context.Context, req *SetOrderExpiryRequest) error
 	ResetOrderExpiryConfig(ctx context.Context) error
