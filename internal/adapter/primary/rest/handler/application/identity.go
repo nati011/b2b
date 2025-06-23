4f98b746ad54952c78cefbaf36d1c15cd4989ec0
natnael jemaneh <njemaneh@chegebeya.com>
2025-06-23 18:09:05 +0300

    + use userId in context for identity contexts

--- internal/adapter/primary/rest/handler/application/identity.go
+++ internal/adapter/primary/rest/handler/application/identity.go
@@ -1,8 +1,8 @@
 package application
 
 import (
+	"errors"
 	"net/http"
-	"strconv"
 	"time"
 
 	"b2b.nati011.github.com/internal/adapter/primary/rest/handler"
@@ -54,67 +54,63 @@ func (i *IdentityHandler) Routes(mux *http.ServeMux) {
 }
 
 func (a *IdentityHandler) GetIdentity(w http.ResponseWriter, r *http.Request) {
-	const ParamId = "id"
-	paramValues := r.URL.Query()
-	paramIdValue := paramValues.Get(ParamId)
-	if paramIdValue != "" {
-		typedParamId, err := strconv.Atoi(paramIdValue)
-		if err != nil {
+	userId, ok := r.Context().Value("userId").(int)
+	if !ok {
+		util.ServerErrorResponse(w, errors.New("userId not found in context or is not an integer"))
+		return
+	}
+
+	user_resp, err := a.userService.Get(r.Context(), userId)
+	if err != nil {
+		switch err {
+		case user.ErrUnknown:
+			util.ServerErrorResponse(w, err)
+			return
+		default:
 			util.RequestErrorResponse(w, err)
 			return
 		}
-		user_resp, err := a.userService.Get(r.Context(), typedParamId)
-		if err != nil {
-			switch err {
-			case user.ErrUnknown:
-				util.ServerErrorResponse(w, err)
-				return
-			default:
-				util.RequestErrorResponse(w, err)
-				return
-			}
-		}
-
-		role_resp, err := a.userService.GetAllAssignedRoles(r.Context(), typedParamId)
-		if err != nil {
-			switch err {
-			case user.ErrUnknown:
-				util.ServerErrorResponse(w, err)
-				return
-			case user.ErrNoRoleAssigned:
-			default:
-				util.RequestErrorResponse(w, err)
-				return
-			}
-		}
-
-		allowed_resources := role.GetAllResourcesResponse{}
-		for _, i := range role_resp.List {
-			resource_resp, err := a.roleService.GetAllResources(r.Context(), i.Id)
-			if err != nil {
-				switch err {
-				case resource.ErrUnknown:
-					util.ServerErrorResponse(w, err)
-					return
-				default:
-					util.RequestErrorResponse(w, err)
-					return
-				}
-			}
-			allowed_resources.List = append(allowed_resources.List, resource_resp.List...)
-		}
-		response := GetIdentityResponse{
-			Id:          user_resp.Id,
-			FirstName:   user_resp.FirstName,
-			LastName:    user_resp.LastName,
-			Email:       user_resp.Email,
-			Phone:       user_resp.Phone,
-			Username:    user_resp.Username,
-			DOB:         user_resp.DOB,
-			IsActive:    user_resp.IsActive,
-			ExternalId:  user_resp.ExternalId,
-			Permissions: allowed_resources,
-		}
-		util.OperationSuccessResponse(w, util.Envelope{"user": response})
 	}
+
+	role_resp, err := a.userService.GetAllAssignedRoles(r.Context(), userId)
+	if err != nil {
+		switch err {
+		case user.ErrUnknown:
+			util.ServerErrorResponse(w, err)
+			return
+		case user.ErrNoRoleAssigned:
+		default:
+			util.RequestErrorResponse(w, err)
+			return
+		}
+	}
+
+	allowed_resources := role.GetAllResourcesResponse{}
+	for _, i := range role_resp.List {
+		resource_resp, err := a.roleService.GetAllResources(r.Context(), i.Id)
+		if err != nil {
+			switch err {
+			case resource.ErrUnknown:
+				util.ServerErrorResponse(w, err)
+				return
+			default:
+				util.RequestErrorResponse(w, err)
+				return
+			}
+		}
+		allowed_resources.List = append(allowed_resources.List, resource_resp.List...)
+	}
+	response := GetIdentityResponse{
+		Id:          user_resp.Id,
+		FirstName:   user_resp.FirstName,
+		LastName:    user_resp.LastName,
+		Email:       user_resp.Email,
+		Phone:       user_resp.Phone,
+		Username:    user_resp.Username,
+		DOB:         user_resp.DOB,
+		IsActive:    user_resp.IsActive,
+		ExternalId:  user_resp.ExternalId,
+		Permissions: allowed_resources,
+	}
+	util.OperationSuccessResponse(w, util.Envelope{"user": response})
 }
