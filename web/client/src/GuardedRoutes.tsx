c3dd8c34dfbce2a3126b5ef45762968a5dedd23f
expired-pineapple <ruthtirusew944@gmail.com>
2025-06-26 18:22:47 +0300

    Client side frontend refactor init:

--- web/client/src/GuardedRoutes.tsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-export const PrivateRoutes = () => {
-    let auth = { 'token': true }
-    return (
-        auth.token ? <Outlet /> : <Navigate to='/login' />
-    )
-}
