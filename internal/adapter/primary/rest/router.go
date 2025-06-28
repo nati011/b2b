d61d17379e318b2c5b1424b73d468814e30ac251
natnael jemaneh <njemaneh@chegebeya.com>
2025-06-28 16:35:15 +0300

    + resolve fetch resource error handling issue

--- internal/adapter/primary/rest/router.go
+++ internal/adapter/primary/rest/router.go
@@ -2,7 +2,6 @@ package rest
 
 import (
 	"context"
-	"log"
 	"net/http"
 
 	"b2b.nati011.github.com/internal/adapter/primary/rest/handler"
@@ -46,14 +45,22 @@ func BuildRouter(mux *http.ServeMux, applicationServices *application_core.Conta
 
 	ctx := context.Background()
 	for _, r := range handler.GetRoutes() {
-
-		_, err := applicationServices.ResourceService.Create(ctx, &resource.CreateRequest{
+		_, err := applicationServices.ResourceService.GetByName(ctx, r.Name)
+		if err != nil {
+			switch err {
+			case resource.ErrNameNotFound:
+				continue
+			default:
+				panic("Failed to fetch resource")
+			}
+		}
+		_, err = applicationServices.ResourceService.Create(ctx, &resource.CreateRequest{
 			Name:     r.Name,
 			Resource: r.Resource,
 			Action:   resource.ANY,
 		})
 		if err != nil {
-			log.Printf("Failed to create resource err: %v", err)
+			panic("Failed to create resource err")
 		}
 	}
 	return nil
