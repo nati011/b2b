914eda3cce4b165efe4db7528c6a421b836fbc93
expired-pineapple <ruthtirusew944@gmail.com>
2025-06-30 15:19:47 +0300

    + profile update and identity fetch on web clients

--- web/admin/app/(dashboard)/settings/page.tsx
+++ web/admin/app/(dashboard)/settings/page.tsx
@@ -1,84 +1,145 @@
-'use client'
-import useProfileStore from "@/app/libs/store/useProfileStore"
-import { useEffect, useState } from "react";
-import { Button, Card, Form, Input } from "antd";
-import { VscAccount } from "react-icons/vsc";
-import { DatePicker, Space } from 'antd';
-import type { DatePickerProps, GetProps } from 'antd';
+"use client"
+import { FormEvent, useEffect, useState } from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { UserAccount, UserIdentity } from '@/app/libs/types';
+import { Button } from "@/components/ui/button";
+import { PiSpinner } from "react-icons/pi";
+import { useUserStore } from "@/app/libs/store/useAuthStore";
+import { toast } from "sonner";
 
 export default function Settings() {
     const {
-        profile,
+        user,
         loading,
+        success,
         error,
-        fetchProfile
-    } = useProfileStore()
-    const [form] = Form.useForm();
-    const [success, setSuccesss] = useState(true)
+        fetchUser,
+        updateProfile
+    } = useUserStore()
 
+    const [formData, setFormData] = useState<Partial<UserIdentity>>({
+        first_name: "",
+        last_name: "",
+        email: "",
+        phone: "",
+        username: "",
+        dob: "",
+    });
 
-    type RangePickerProps = GetProps<typeof DatePicker.RangePicker>;
-
-    const { RangePicker } = DatePicker;
-
-    const onOk = (value: DatePickerProps['value'] | RangePickerProps['value']) => {
-        console.log('onOk: ', value);
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }));
     };
 
+    useEffect(()=>{
+        fetchUser()
+    },[])
+    useEffect(()=>{
+        if(user){
+        setFormData(user)
+    }
+    },[user])
 
-    useEffect(() => {
-        fetchProfile();
-    }, []);
+    useEffect(()=>{
+        if(success){
+            toast.success(success)
+        }
+        if(error){
+            toast.error(error)
+        }
+    },[error,success ])
 
     return (
-        <Card title="Account Settings">
-            <Form
-                layout={'vertical'}
-                form={form}
-            >
-                <div className="flex gap-8 w-full">
-                    <div className="">
-                        <VscAccount className="text-9xl text-gray-700" />
-                    </div>
-                    <div className="grid grid-cols-1 w-full">
-                        <Form.Item label="First Name">
-                            <Input placeholder="First Name" />
-                        </Form.Item>
-                        <Form.Item label="Last Name">
-                            <Input placeholder="Last Name" />
-                        </Form.Item>
-                    </div>
+        <div className="flex flex-col gap-4 p-6 md:p-10 my-32">
+            <div className="flex flex-1 items-center justify-center">
+                <div className="w-full max-w-lg">
+                    <form className="flex flex-col gap-6" >
+                        <div className="flex flex-col items-center gap-2 text-left">
+                            <h1 className="text-2xl font-bold">Profile Settings</h1>
+                            <p className="text-balance text-sm text-muted-foreground">
+                                Enter your details to update your account
+                            </p>
+                        </div>
+
+                        <div className="grid gap-6">
+                            {error && (
+                                <div className="bg-red-100/[0.2] rounded-md p-2 border border-red-900">
+                                    <p className="text-red-900 text-center font-medium">{error}</p>
+                                </div>
+                            )}
+
+                            {/* Personal Information */}
+                            <div className="grid grid-cols-2 gap-4">
+                                <div className="grid gap-2">
+                                    <Label htmlFor="first_name">First Name<span className='text-red-500'>*</span></Label>
+                                    <Input
+                                        id="first_name"
+                                        name="first_name"
+                                        placeholder="John"
+                                        required
+                                        value={formData.first_name}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="grid gap-2">
+                                    <Label htmlFor="last_name">Last Name<span className='text-red-500'>*</span></Label>
+                                    <Input
+                                        id="last_name"
+                                        name="last_name"
+                                        placeholder="Doe"
+                                        required
+                                        value={formData.last_name}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                            </div>
+
+                            {/* Contact Information */}
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email*</Label>
+                                <Input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    placeholder="m@example.com"
+                                    required
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                />
+                            </div>
+
+                            <div className="grid gap-2">
+                                <Label htmlFor="phone">Phone Number<span className='text-red-500'>*</span></Label>
+                                <Input
+                                    id="phone"
+                                    name="phone"
+                                    type="tel"
+                                    placeholder="+251966961629"
+                                    required
+                                    value={formData.phone}
+                                    onChange={handleChange}
+                                />
+                            </div>
+
+
+                            <Button onClick={()=>{updateProfile(formData)}} className="w-full" disabled={loading}>
+                                {loading ? (
+                                    <div className="flex gap-2">
+                                        <PiSpinner className="animate-spin" />
+                                        <span>Updating account...</span>
+                                    </div>
+                                ) : (
+                                    <span>Update Profile</span>
+                                )}
+                            </Button>
+                        </div>
+                    </form>
                 </div>
-                <Form.Item label="Email">
-                    <Input placeholder="Email" />
-                </Form.Item>
-                <Form.Item label="Phone">
-                    <Input placeholder="Phone" />
-                </Form.Item>
-                <Form.Item label="Username">
-                    <Input placeholder="Username" />
-                </Form.Item>
-                <Form.Item label="BirthDate">
-                    <DatePicker
-                        className="w-full"
-                        onChange={(value, dateString) => {
-                            console.log('Selected Time: ', value);
-                            console.log('Formatted Selected Time: ', dateString);
-                        }}
-                        onOk={onOk}
-                    />
-                </Form.Item>
-            </Form>
-            <div style={{ marginTop: 24 }} className="flex w-full justify-end">
-                <Button style={{ margin: '0 8px' }}>
-                    Cancel
-                </Button>
-
-                <Button type="primary" onClick={() => setSuccesss(true)} className="px-10 border border-[#451f87]">
-                    Submit
-                </Button>
             </div>
-        </Card>
-
+        </div>
     );
 }
