0feca7d3429dbda94de82b56ae786f2280d53e68
natnael jemaneh <njemaneh@chegebeya.com>
2025-07-03 11:31:42 +0300

    + resolve router issue

--- internal/adapter/primary/rest/router.go
+++ internal/adapter/primary/rest/router.go
@@ -60,13 +60,12 @@ func BuildRouter(mux *http.ServeMux, applicationServices *application_core.Conta
 			Action:   resource.ANY,
 		})
 		if err != nil {
-
 			if _, err = applicationServices.ResourceService.Create(ctx,
 				&resource.CreateRequest{
 					Name: r.Name, Resource: r.Resource, Action: resource.ANY}); err != nil {
 				panic("Failed to create resource err")
 			}
 		}
-		return nil
 	}
+	return nil
 }
