c38dc57177bf8a91828704dd0c5d911232f18537
ship <natnaeljemaneh01@gmail.com>
2025-08-24 09:32:38 +0300

    + fix distributor test

--- internal/core/domain/product/test/integration/distributor/distributor_test.go
+++ internal/core/domain/product/test/integration/distributor/distributor_test.go
@@ -5,10 +5,12 @@ import (
 	"os"
 	"testing"
 
+	"b2b.nati011.github.com/internal/core/domain/distributor"
 	"b2b.nati011.github.com/internal/core/domain/product"
 )
 
 var testContainer product.TestContainer
+var distributorId int
 
 func TestMain(m *testing.M) {
 	setup()
@@ -18,6 +20,27 @@ func TestMain(m *testing.M) {
 
 func setup() {
 	testContainer = product.NewPackageIntegrationTestContainer()
+	var err error
+	ctx := context.Background()
+	distributorId, err = testContainer.DistributorService.Create(ctx, &distributor.CreateRequest{
+		Tin:         "1111111111",
+		Latitude:    "9.0192° N",
+		Longitude:   "38.7525° E",
+		GeneralZone: "test",
+		Region:      "test",
+		Woreda:      "test",
+		Username:    "username",
+		FirstName:   "test",
+		LastName:    "test",
+		Email:       "test@gmail.com",
+	})
+	if err != nil {
+		panic("failed to create distributor")
+	}
+	err = testContainer.DistributorService.Activate(ctx, distributorId)
+	if err != nil {
+		panic("failed to activate distributor")
+	}
 }
 
 func teardown() {
@@ -63,7 +86,6 @@ func Test_Validate_Distributor(t *testing.T) {
 			Attributes: map[string]string{
 				"test": "test",
 			},
-			DistributorId: distributorId,
 		})
 		wantErr := product.ErrDistributorIdMandatory
 		if err != wantErr {
