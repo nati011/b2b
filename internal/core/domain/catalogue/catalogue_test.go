85e7dd7f2fdb1ace693f2212c01b77f7c04430d7
ship <natnaeljemaneh01@gmail.com>
2025-08-24 11:02:37 +0300

    + add catalogue test container

---
+++ internal/core/domain/catalogue/catalogue_test.go
@@ -0,0 +1,28 @@
+package catalogue
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+	os.Exit(code)
+	setup()
+}
+
+func setup() {
+
+}
+
+func tearDown() {
+
+}
+
+func Test_GetAll(t *testing.T) {
+
+}
+
+func Test_Search(t *testing.T) {
+
+}
