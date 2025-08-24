526a81cbe26844cda4dd7f39ad48878847ee578c
ship <natnaeljemaneh01@gmail.com>
2025-08-24 09:17:47 +0300

    + build event handler

---
+++ cmd/init_event_listener.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"errors"
+
+	event "b2b.nati011.github.com/internal/adapter/primary/event/handler"
+	application_core "b2b.nati011.github.com/internal/core/application"
+	domain_core "b2b.nati011.github.com/internal/core/domain"
+)
+
+var (
+	ErrFailedToBuildEvent = errors.New(" failed to init event lister")
+)
+
+func InitEventLister(applicationServices *application_core.Container, domainServices *domain_core.Container) error {
+	err := event.BuildEventHandler(applicationServices, domainServices)
+	if err != nil {
+		return err
+	}
+	return nil
+}
