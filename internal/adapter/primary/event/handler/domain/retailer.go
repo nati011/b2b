88acc7d85a3a659ac4d953fb3b4bbdb7f24b5356
ship <natnaeljemaneh01@gmail.com>
2025-08-26 23:18:11 +0300

    + upon sso raise an event

---
+++ internal/adapter/primary/event/handler/domain/retailer.go
@@ -0,0 +1,43 @@
+package domain_handler
+
+import (
+	"context"
+	"fmt"
+
+	"b2b.nati011.github.com/internal/core/application/event"
+	"b2b.nati011.github.com/internal/core/domain/retailer"
+	"b2b.nati011.github.com/internal/core/util"
+)
+
+type AuthHandler struct {
+	service retailer.Provider
+	event   *event.Broker
+}
+
+func InitAuth(service retailer.Provider, event *event.Broker) error {
+	handler := &AuthHandler{
+		service: service,
+		event:   event,
+	}
+
+	ch := event.Subscribe(util.EVENT_RETAILER_SSO)
+
+	// Start a goroutine to listen for events asynchronously
+	go func() {
+		for msg := range ch {
+			payload, ok := msg.(util.EventRetailerSSOPayload) // or whatever type you publish
+			if !ok {
+				fmt.Printf("Received unexpected event type: %T\n", msg)
+				continue
+			}
+			ctx := context.Background()
+			handler.service.CreateAssisted(ctx, &retailer.CreateAssistedRequest{
+				FirstName: payload.FirstName,
+				LastName:  payload.LastName,
+				Email:     payload.Email,
+				Username:  payload.Username,
+			})
+		}
+	}()
+	return nil
+}
--- internal/adapter/primary/rest/handler/application/auth.go
+++ internal/adapter/primary/rest/handler/application/auth.go
@@ -9,6 +9,7 @@ import (
 	util "b2b.nati011.github.com/internal/adapter/primary/rest/handler/util"
 	auth "b2b.nati011.github.com/internal/core/application/auth"
 	"b2b.nati011.github.com/internal/core/application/middleware"
+	oauth "b2b.nati011.github.com/internal/core/application/oauth"
 	"b2b.nati011.github.com/internal/core/application/resource"
 
 	application_core "b2b.nati011.github.com/internal/core/application"
@@ -18,6 +19,7 @@ import (
 type AuthHandler struct {
 	authMiddleware middleware.Auth
 	service        auth.Provider
+	oauthService   oauth.Provider
 }
 
 type ResetPasswordRequest struct {
@@ -50,6 +52,11 @@ func initRegisterResources() {
 		Action:   "ALL",
 		Resource: "/api/v1/auth/reset/{param}",
 	})
+	handler.RegisterResource(resource.CreateRequest{
+		Name:     "sso",
+		Action:   "ALL",
+		Resource: "/api/v1/auth/sso",
+	})
 }
 
 func (a *AuthHandler) Init(authMiddleWare *middleware.Auth, services *application_core.Container, domainService *domain_core.Container) error {
@@ -62,6 +69,10 @@ func (a *AuthHandler) Routes(mux *http.ServeMux) {
 
 	mux.HandleFunc("POST /api/v1/auth/login", func(w http.ResponseWriter, r *http.Request) {
 		a.authMiddleware.RequireNoAuthentication(http.HandlerFunc(a.LoginHandler)).ServeHTTP(w, r)
+	})
+
+	mux.HandleFunc("POST /api/v1/auth/sso", func(w http.ResponseWriter, r *http.Request) {
+		a.authMiddleware.RequireNoAuthentication(http.HandlerFunc(a.SSOHandler)).ServeHTTP(w, r)
 	})
 
 	mux.HandleFunc("POST /api/v1/auth/logout", func(w http.ResponseWriter, r *http.Request) {
@@ -104,6 +115,10 @@ func (h *AuthHandler) ResetCredentialsHandler(w http.ResponseWriter, r *http.Req
 		return
 	}
 	util.OperationSuccessMessageResponse(w, "password reset successfully")
+}
+
+func (a *AuthHandler) SSOHandler(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func (a *AuthHandler) LoginHandler(w http.ResponseWriter, r *http.Request) {
