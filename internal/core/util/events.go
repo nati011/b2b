1dc14723e2a9f47317d6391313f1392639a1c853
ship <natnaeljemaneh01@gmail.com>
2025-08-26 23:18:11 +0300

    + upon sso raise an event

--- internal/core/util/events.go
+++ internal/core/util/events.go
@@ -2,4 +2,13 @@ package util
 
 var (
 	EVENT_DISTRIBUTOR_DEACTIVATE = "event_distributor_disable"
+	EVENT_RETAILER_SSO           = "event_retailer_signon"
 )
+
+type EventRetailerSSOPayload struct {
+	ID        string `json:"id"`
+	Email     string `json:"email"`
+	Username  string `json:"username"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
