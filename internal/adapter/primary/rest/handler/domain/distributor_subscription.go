1b1e2cf07f1b5d0857e794c4932ed15dc37bfb95
Ruth Tirusew <87856799+ruth-tirusew@users.noreply.github.com>
2025-08-28 17:01:12 +0300

    Update distributor_subscription.go

--- internal/adapter/primary/rest/handler/domain/distributor_subscription.go
+++ internal/adapter/primary/rest/handler/domain/distributor_subscription.go
@@ -61,7 +61,7 @@ func (d *Distributor_Subscription) Routes(mux *http.ServeMux) {
 	})
 
 	mux.HandleFunc("POST /api/v1/subscription", func(w http.ResponseWriter, r *http.Request) {
-		d.authMiddleware.RequireAuthentication(http.HandlerFunc(d.CreateSubscriptionHandler)).ServeHTTP(w, r)
+		d.authMiddleware.RequireNoAuthentication(http.HandlerFunc(d.CreateSubscriptionHandler)).ServeHTTP(w, r)
 	})
 
 	mux.HandleFunc("GET /api/v1/plan", func(w http.ResponseWriter, r *http.Request) {
