374fb9d6d824326282fd8e92ae15700454157ee2
ruth-tirusew <ruthtirusew944@gmail.com>
2025-08-29 18:41:04 +0300

    fix: configurable product form

--- web/admin/lib/constants.ts
+++ web/admin/lib/constants.ts
@@ -1,5 +1,6 @@
 export const ACL: Record<string, string[]> = {
     '/': ['order', 'order_distributors'],
+    '/products/configurable' :['configurable_product'],
     '/distributors':['distributor'],
     '/distributors/agents':['distributor_user'],
     '/distributor/agents/form':['create_distributor_user'],
