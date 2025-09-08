df239cffc1ed54e988c4eb68a757a34327434ce2
ship <natnaeljemaneh01@gmail.com>
2025-09-08 14:29:36 +0300

    + resolve issue

--- internal/adapter/primary/cron/domain/order_expiry/order_expiry.go
+++ internal/adapter/primary/cron/domain/order_expiry/order_expiry.go
@@ -78,7 +78,7 @@ func CancelExpiredOrders(
 		if partner.PaymentMethod == payment_partner.PAYMENT_METHOD_DIGITAL && o.PaymentStatus == order.PAYMENT_PENDING_STATUS {
 			// cancel order with digital payment option after x period if unsettled
 			if now.Sub(o.CreatedAt) > time.Duration(expireAfterDurationInMinutes) {
-				log.Printf("# Canceling order Id: %v Placed by retailerId: %v | retailerName: %v", o.RetailerId, o.RetailerName)
+				log.Printf("# Canceling orderId: %v Placed by retailerId: %v | retailerName: %v", o.Id, o.RetailerId, o.RetailerName)
 				err = orderService.Cancel(ctx, o.Id)
 				if err != nil {
 					log.Printf("Failed to cancel order err: %v Order ID: %v", err, o.Id)
@@ -88,7 +88,7 @@ func CancelExpiredOrders(
 			// cancel order with manual payment option after x period if unconfirmed(manual order confirmation)…
 
 			if now.Sub(o.CreatedAt) > time.Duration(expireAfterDurationInMinutes) && o.ConfirmationStatus != order.ORDER_CONFIRMED {
-				log.Printf("# Canceling order Id: %v Placed by retailerId: %v | retailerName: %v", o.RetailerId, o.RetailerName)
+				log.Printf("# Canceling order Id: %v Placed by retailerId: %v | retailerName: %v", o.Id, o.RetailerId, o.RetailerName)
 				err = orderService.Cancel(ctx, o.Id)
 				if err != nil {
 					log.Printf("Failed to cancel order err: %v Order ID: %v", err, o.Id)
