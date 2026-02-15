1cafd714bf50c88fe7352f70a543e7555a9eef1e
natnaelj <natnaeljemaneh001@gmail.com>
2026-02-15 08:19:33 +0300

    fix: remove web

--- web/client/app/SessionProvider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import { SessionProvider as AuthSessionProvider } from "next-auth/react";
-
-const SessionProvider = ({ children }: { children: React.ReactNode }) => {
-  return <AuthSessionProvider>{children}</AuthSessionProvider>;
-};
-
-export default SessionProvider;
